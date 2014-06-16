007a1e780a3e61c95ebecfbe7984beb3b9cdf0f2
gjbex <geertjan.bex@uhasselt.be>
2014-06-16 10:39:40 +0200

    Added some more functions to time

--- Fortran/Functions/math_func_mod.f90
+++ Fortran/Functions/math_func_mod.f90
@@ -17,12 +17,18 @@ contains
 
     subroutine setup()
         implicit none
-        functions(1)%f => f1
-        functions(1)%name = "x**2"
-        functions(2)%f => f2
-        functions(2)%name = "sqrt(x)"
-        functions(3)%f => f3
-        functions(3)%name = "exp(x)"
+        functions(1)%f => f6
+        functions(1)%name = "x*x"
+        functions(2)%f => f1
+        functions(2)%name = "x**2"
+        functions(3)%f => f5
+        functions(3)%name = "x**2.1D00"
+        functions(4)%f => f2
+        functions(4)%name = "sqrt(x)"
+        functions(5)%f => f3
+        functions(5)%name = "exp(x)"
+        functions(6)%f => f4
+        functions(6)%name = "erf(x)"
     end subroutine setup
 
     function f1(x) result(r)
@@ -45,5 +51,26 @@ contains
         double precision :: r
         r = exp(x)
     end function f3
+    
+    function f4(x) result(r)
+        implicit none
+        double precision, intent(in) :: x
+        double precision :: r
+        r = erf(x)
+    end function f4
+    
+    function f5(x) result(r)
+        implicit none
+        double precision, intent(in) :: x
+        double precision :: r
+        r = x**2.1D00
+    end function f5
+    
+    function f6(x) result(r)
+        implicit none
+        double precision, intent(in) :: x
+        double precision :: r
+        r = x*x
+    end function f6
     
 end module math_func_mod
