0e369ea73d14009e0a5a35e2873b733463ae5fdc
Geert Jan BEX <geertjan.bex@uhasselt.be>
2014-06-17 17:36:01 +0200

    Added timing spreadsheet; added real precision test

---
+++ Fortran/Functions/precision_test.f90
@@ -0,0 +1,14 @@
+program precision_test
+    use, intrinsic :: iso_fortran_env
+    implicit none
+    integer, parameter :: sp = kind(1.0), &
+                          dp = selected_real_kind(2*precision(1.0_sp))
+
+    print *, "sp = ", sp
+    print *, "dp = ", dp
+
+    print *, "REAL32 = ", REAL32
+    print *, "REAL64 = ", REAL64
+
+end program precision_test
+
