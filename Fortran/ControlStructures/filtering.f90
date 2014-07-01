ba3b7271bd428557cefc842b685f14cc2aeda81a
Geert Jan BEX <geertjan.bex@uhasselt.be>
2014-07-01 16:26:30 +0200

    Added exaples for forall and where control structures

---
+++ Fortran/ControlStructures/filtering.f90
@@ -0,0 +1,20 @@
+program filtering
+use, intrinsic :: iso_fortran_env
+implicit none
+integer, parameter :: N = 5, sp = REAL32
+real(kind=sp), dimension(N, N) :: A
+integer :: i, j
+character(len=40) :: fmt_str
+
+write (fmt_str, "('(', I2, 'F5.2)')") N
+
+A = 0.0_sp
+forall (i = 1:N, j = 1:N, i == j - 1 .or. i == j + 1)
+    A(i, j) = 1.0_sp
+end forall
+
+do i = 1, N
+    print fmt_str, A(i, :)
+end do
+
+end program filtering
