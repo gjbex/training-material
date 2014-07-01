bc261182f6812b6a47933ea48605162a69b251e2
Geert Jan BEX <geertjan.bex@uhasselt.be>
2014-07-01 17:24:17 +0200

    Added recursion example

---
+++ Fortran/Functions/recursion.f90
@@ -0,0 +1,32 @@
+module funcs_mod
+    implicit none
+
+    private
+    public :: factorial
+
+contains
+
+    recursive function factorial(n) result(f)
+        implicit none
+        integer, intent(in) :: n
+        integer :: f
+        if (n < 2) then
+            f = 1
+        else
+            f = n*factorial(n - 1)
+        end if
+    end function factorial
+
+end module funcs_mod
+
+program recursion
+    use funcs_mod
+    implicit none
+    integer, parameter :: n_min = 0, n_max = 6
+    integer :: i
+
+    do i = n_min, n_max
+        print *, i, factorial(i)
+    end do
+
+end program recursion
