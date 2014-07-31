44acfcebd0b5b32e7e9442f00b599f59a10ed252
gjbex <geertjan.bex@uhasselt.be>
2014-07-31 17:32:54 +0200

    Added log function

--- Fortran/Functions/precision_test.f90
+++ Fortran/Functions/precision_test.f90
@@ -2,7 +2,7 @@ program precision_test
     use, intrinsic :: iso_fortran_env
     implicit none
     integer, parameter :: sp = REAL32, dp = REAL64
-    integer, parameter :: rp = dp
+    integer, parameter :: rp = sp
     integer, parameter :: x_dim = 1000, nr_outer = 10
     real(kind=rp), dimension(x_dim) :: x1, x2, x3
     integer :: i, j, k, p
@@ -198,6 +198,32 @@ program precision_test
                 do k = 1, x_dim
                     x = x_l3 + x3(k)
                     t3 = t3 + exp(x)
+                end do
+                t2 = t2 + t3
+            end do
+            t1 = t1 + t2
+        end do
+        total = total + t1
+    end do
+    call cpu_time(t_end)
+    print r_fmt_str, trim(comp_str), ",", t_end - t_start, ",", total
+
+    comp_str = "log(x)"
+    print c_fmt_str, trim(comp_str)
+    total = 0.0_rp
+    call cpu_time(t_start)
+    do p = 1, nr_outer
+        t1 = 0.0_rp
+        x_l1 = p*0.001_rp
+        do i = 1, x_dim
+            t2 = 0.0_rp
+            x_l2 = x_l1 + x1(i)
+            do j = 1, x_dim
+                t3 = 0.0_rp
+                x_l3 = x_l2 + x2(j)
+                do k = 1, x_dim
+                    x = x_l3 + x3(k)
+                    t3 = t3 + log(x)
                 end do
                 t2 = t2 + t3
             end do
