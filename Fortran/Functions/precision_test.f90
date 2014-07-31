214d68761cd3a5b087f2f7251b6e37f083608fce
Geert Jan BEX <geertjan.bex@uhasselt.be>
2014-07-31 07:17:36 +0200

    Added more operations for precision test

--- Fortran/Functions/precision_test.f90
+++ Fortran/Functions/precision_test.f90
@@ -21,6 +21,33 @@ program precision_test
         x3(i) = x
     end do
 
+    comp_str = "x + y"
+    print c_fmt_str, trim(comp_str)
+    total = 0.0_rp
+    call cpu_time(t_start)
+    do p = 1, nr_outer
+        t1 = 0.0_rp
+        x_l1 = p*0.001_rp
+        do i = 1, x_dim
+            t2 = 0.0_rp
+            x_l2 = x_l1 + x1(i)
+            do j = 1, x_dim
+                t3 = 0.0_rp
+                x_l3 = x_l2 + x2(j)
+                do k = 1, x_dim
+                    x = x_l3 + x3(k)
+                    y = x2(k)
+                    t3 = t3 + x + y
+                end do
+                t2 = t2 + t3
+            end do
+            t1 = t1 + t2
+        end do
+        total = total + t1
+    end do
+    call cpu_time(t_end)
+    print r_fmt_str, trim(comp_str), ",", t_end - t_start, ",", total
+
     comp_str = "x*y"
     print c_fmt_str, trim(comp_str)
     total = 0.0_rp
@@ -35,9 +62,168 @@ program precision_test
                 t3 = 0.0_rp
                 x_l3 = x_l2 + x2(j)
                 do k = 1, x_dim
-                    x = x_l3 + x3(i)
+                    x = x_l3 + x3(k)
                     y = x2(k)
                     t3 = t3 + x*y
+                end do
+                t2 = t2 + t3
+            end do
+            t1 = t1 + t2
+        end do
+        total = total + t1
+    end do
+    call cpu_time(t_end)
+    print r_fmt_str, trim(comp_str), ",", t_end - t_start, ",", total
+
+    comp_str = "x*y + x"
+    print c_fmt_str, trim(comp_str)
+    total = 0.0_rp
+    call cpu_time(t_start)
+    do p = 1, nr_outer
+        t1 = 0.0_rp
+        x_l1 = p*0.001_rp
+        do i = 1, x_dim
+            t2 = 0.0_rp
+            x_l2 = x_l1 + x1(i)
+            do j = 1, x_dim
+                t3 = 0.0_rp
+                x_l3 = x_l2 + x2(j)
+                do k = 1, x_dim
+                    x = x_l3 + x3(k)
+                    y = x2(k)
+                    t3 = t3 + x*y + x
+                end do
+                t2 = t2 + t3
+            end do
+            t1 = t1 + t2
+        end do
+        total = total + t1
+    end do
+    call cpu_time(t_end)
+    print r_fmt_str, trim(comp_str), ",", t_end - t_start, ",", total
+
+    comp_str = "x/y"
+    print c_fmt_str, trim(comp_str)
+    total = 0.0_rp
+    call cpu_time(t_start)
+    do p = 1, nr_outer
+        t1 = 0.0_rp
+        x_l1 = p*0.001_rp
+        do i = 1, x_dim
+            t2 = 0.0_rp
+            x_l2 = x_l1 + x1(i)
+            do j = 1, x_dim
+                t3 = 0.0_rp
+                x_l3 = x_l2 + x2(j)
+                do k = 1, x_dim
+                    x = x_l3 + x3(k)
+                    y = x2(k)
+                    t3 = t3 + x/y
+                end do
+                t2 = t2 + t3
+            end do
+            t1 = t1 + t2
+        end do
+        total = total + t1
+    end do
+    call cpu_time(t_end)
+    print r_fmt_str, trim(comp_str), ",", t_end - t_start, ",", total
+
+    comp_str = "x**y"
+    print c_fmt_str, trim(comp_str)
+    total = 0.0_rp
+    call cpu_time(t_start)
+    do p = 1, nr_outer
+        t1 = 0.0_rp
+        x_l1 = p*0.001_rp
+        do i = 1, x_dim
+            t2 = 0.0_rp
+            x_l2 = x_l1 + x1(i)
+            do j = 1, x_dim
+                t3 = 0.0_rp
+                x_l3 = x_l2 + x2(j)
+                do k = 1, x_dim
+                    x = x_l3 + x3(k)
+                    y = x2(k)
+                    t3 = t3 + x**y
+                end do
+                t2 = t2 + t3
+            end do
+            t1 = t1 + t2
+        end do
+        total = total + t1
+    end do
+    call cpu_time(t_end)
+    print r_fmt_str, trim(comp_str), ",", t_end - t_start, ",", total
+
+    comp_str = "sqrt(x)"
+    print c_fmt_str, trim(comp_str)
+    total = 0.0_rp
+    call cpu_time(t_start)
+    do p = 1, nr_outer
+        t1 = 0.0_rp
+        x_l1 = p*0.001_rp
+        do i = 1, x_dim
+            t2 = 0.0_rp
+            x_l2 = x_l1 + x1(i)
+            do j = 1, x_dim
+                t3 = 0.0_rp
+                x_l3 = x_l2 + x2(j)
+                do k = 1, x_dim
+                    x = x_l3 + x3(k)
+                    t3 = t3 + sqrt(x)
+                end do
+                t2 = t2 + t3
+            end do
+            t1 = t1 + t2
+        end do
+        total = total + t1
+    end do
+    call cpu_time(t_end)
+    print r_fmt_str, trim(comp_str), ",", t_end - t_start, ",", total
+
+    comp_str = "exp(x)"
+    print c_fmt_str, trim(comp_str)
+    total = 0.0_rp
+    call cpu_time(t_start)
+    do p = 1, nr_outer
+        t1 = 0.0_rp
+        x_l1 = p*0.001_rp
+        do i = 1, x_dim
+            t2 = 0.0_rp
+            x_l2 = x_l1 + x1(i)
+            do j = 1, x_dim
+                t3 = 0.0_rp
+                x_l3 = x_l2 + x2(j)
+                do k = 1, x_dim
+                    x = x_l3 + x3(k)
+                    t3 = t3 + exp(x)
+                end do
+                t2 = t2 + t3
+            end do
+            t1 = t1 + t2
+        end do
+        total = total + t1
+    end do
+    call cpu_time(t_end)
+    print r_fmt_str, trim(comp_str), ",", t_end - t_start, ",", total
+
+    comp_str = "erf(x)"
+    print c_fmt_str, trim(comp_str)
+    total = 0.0_rp
+    call cpu_time(t_start)
+    do p = 1, nr_outer
+        t1 = 0.0_rp
+        x_l1 = p*0.001_rp
+        do i = 1, x_dim
+            t2 = 0.0_rp
+            x_l2 = x_l1 + x1(i)
+            do j = 1, x_dim
+                t3 = 0.0_rp
+                x_l3 = x_l2 + x2(j)
+                do k = 1, x_dim
+                    x = x_l3 + x3(k)
+                    t3 = t3 + erf(x)
                 end do
                 t2 = t2 + t3
             end do
