edd8640649e8cc9392515806950756272fbd1d87
Geert Jan BEX <geertjan.bex@uhasselt.be>
2014-08-02 15:05:18 +0200

    Added script to created animated gif

---
+++ Python/Matplotlib/ising.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import numpy as np
+import scipy.optimize as opt
+import matplotlib.pyplot as plt
+
+imgfile_fmt = '{0:s}-{1:04d}.png'
+imgpatt_fmt = '{0:s}-*.png'
+moviefile_fmt = '{0:s}.gif'
+
+def create_sigmoid(beta):
+    def f(x):
+        return np.tanh(beta*x)
+    return f
+        
+def find_roots(f, x_low=-5.0, x_high=5.0):
+    def f_opt(X):
+        return f(X[0]) - X[0]
+    r1 = opt.root(f_opt, np.array([x_low]))
+    r2 = opt.root(f_opt, np.array([x_high]))
+    return np.array([r1.x[0], 0.0, r2.x[0]])
+
+def create_plot(beta, i, options):
+    sigmoid = create_sigmoid(beta)
+    x = np.linspace(-options.x_max, options.x_max, options.points)
+    y = sigmoid(x)
+    plt.plot(x, y)
+    plt.plot(x, x)
+    plt.xlabel(r'$M$')
+    plt.ylabel(r'$\tanh(\beta M)$')
+    plt.axis([-options.x_max, options.x_max, -1.1, 1.1])
+    plt.text(-options.x_max + 0.5, 0.5, r'$\beta = {0:.5f}$'.format(beta),
+             fontsize=16)
+# plot roots
+    roots = find_roots(sigmoid)
+    values = sigmoid(roots)
+    plt.plot(roots, values, 'ro')
+    plt.savefig(imgfile_fmt.format(options.file_base, i))
+    plt.clf()
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    import os, subprocess, sys
+
+    arg_parser = ArgumentParser(description='create movie for Ising mean '
+                                            'field equation')
+    arg_parser.add_argument('--x_max', type=float, default=2.0,
+                            help='x-value range')
+    arg_parser.add_argument('--points', type=int, default=200,
+                            help='number of plot points to use')
+    arg_parser.add_argument('--beta_max', type=float, default=5.0,
+                            help='maximum beta value')
+    arg_parser.add_argument('--alpha', type=float, default=2.0,
+                            help='value in beta(i+1) = 1 + (1 - beta(i))/alpha')
+    arg_parser.add_argument('--file_base', default='sigmoid',
+                            help='base name to use for file')
+    arg_parser.add_argument('--steps', type=int, default=10,
+                            help='number of plot points to use')
+    options = arg_parser.parse_args()
+
+    i = 0
+    beta = options.beta_max
+    while beta >= 1.00001 and i <= options.steps:
+        i += 1
+        msg_str = 'creating plot {0:d} for beta = {1:.5f}\n'.format(i, beta)
+        sys.stderr.write(msg_str)
+        create_plot(beta, i, options)
+        beta = 1.0 + (beta - 1.0)/2.0
+    total_figures = i
+    sys.stderr.write('creating movie GIF\n')
+    try:
+        status = subprocess.call(['convert', '-set', 'delay', '100',
+                                  imgpatt_fmt.format(options.file_base),
+                                  moviefile_fmt.format(options.file_base)])
+    except subprocess.CalledProcessError as e:
+        sys.stderr.write('# error: {0}\n'.format(e.message))
+        sys.exit(status)
+    for i in xrange(1, total_figures + 1):
+        os.remove(imgfile_fmt.format(options.file_base, i))
+
