b5e6be2fa2509abf291bd3cc997638010ad9d7b0
Geert Jan BEX <geertjan.bex@uhasselt.be>
2014-11-30 22:31:20 +0100

    Deleted unnecessary classes

--- Visualization/Synthetic/grid.py
+++
@@ -1,5 +0,0 @@
-from tables import IsDescription, Float64Col
-
-class Grid(IsDescription):
-    point = Float64Col()
-
--- Visualization/Synthetic/scalar_field.py
+++
@@ -1,5 +0,0 @@
-from tables import IsDescription, Float64Col
-
-class ScalarField(IsDescription):
-    value = Float64Col()
-
