daa69d27cb5c693699a7c4dcedc63920654bc05c
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-01-21 12:21:13 +0100

    Renamed parameter to avoid confusion with function name

--- Python/Matplotlib/heatmap_plot.py
+++ Python/Matplotlib/heatmap_plot.py
@@ -2,9 +2,9 @@
 
 from math import exp, sqrt, cos, pi
 
-def f(x, y, x0=0.0, f=1.0, beta=0.5):
+def f(x, y, x0=0.0, freq=1.0, beta=0.5):
     r = sqrt((x - x0)**2 + y**2)
-    return exp(-beta*r)*cos(2.0*pi*f*r)
+    return exp(-beta*r)*cos(2.0*pi*freq*r)
 
 if __name__ == '__main__':
     import numpy as np
