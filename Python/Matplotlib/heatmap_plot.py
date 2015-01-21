6cca86d6b630b1b2ba1ea1974485aa84f49406b6
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-01-21 20:54:43 +0100

    Added command line argument configuration of plot

--- Python/Matplotlib/heatmap_plot.py
+++ Python/Matplotlib/heatmap_plot.py
@@ -7,19 +7,40 @@ def f(x, y, x0=0.0, freq=1.0, beta=0.5):
     return np.exp(-beta*r)*np.cos(2.0*np.pi*freq*r)
 
 if __name__ == '__main__':
+    from argparse import ArgumentParser
     import matplotlib.pyplot as plt
 
-    nr_points = 200
-    x_min = -5.0
-    x_max = 5.0
-    y_min = -5.0
-    y_max = 5.0
-    x = np.linspace(-3.0, 3.0, nr_points)
-    y = np.linspace(-3.0, 3.0, nr_points)
+    arg_parser = ArgumentParser(description='plot heatmap of 2D Gaussians')
+    arg_parser.add_argument('file', nargs='?', help='file name for plot')
+    arg_parser.add_argument('--points', type=int, default=200,
+                            help='number of points on x and y axis')
+    arg_parser.add_argument('--x_min', type=float, default=-5.0,
+                            help='minimal x value')
+    arg_parser.add_argument('--y_min', type=float, default=-5.0,
+                            help='minimal y value')
+    arg_parser.add_argument('--x_max', type=float, default=5.0,
+                            help='maximum x value')
+    arg_parser.add_argument('--y_max', type=float, default=5.0,
+                            help='maximum y value')
+    arg_parser.add_argument('--x0_1', type=float, default=-1.0,
+                            help='x position of first maximum')
+    arg_parser.add_argument('--x0_2', type=float, default=1.0,
+                            help='x position of second maximum')
+    arg_parser.add_argument('--f_1', type=float, default=1.0,
+                            help='frequency of first maximum')
+    arg_parser.add_argument('--f_2', type=float, default=0.5,
+                            help='frequency of second maximum')
+    options = arg_parser.parse_args()
+    x = np.linspace(options.x_min, options.x_max, options.points)
+    y = np.linspace(options.y_min, options.y_max, options.points)
     xx, yy = np.meshgrid(x, y)
-    v1 = f(xx, yy, x0=-1.0)
-    v2 = f(xx, yy, x0=1.0, freq=0.5)
-    plt.imshow(v1 + v2, extent=[x_min, x_max, y_min, y_max])
+    v1 = f(xx, yy, x0=options.x0_1, freq=options.f_1)
+    v2 = f(xx, yy, x0=options.x0_2, freq=options.f_2)
+    plt.imshow(v1 + v2, extent=[options.x_min, options.x_max,
+                                options.y_min, options.y_max])
     plt.grid(True)
-    plt.show()
+    if options.file:
+        plt.savefig(options.file)
+    else:
+        plt.show()
 
