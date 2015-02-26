0c78e19ae11e9ca1b96da33a338aee037b41f576
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-02-26 08:24:24 +0100

    Made data generation code more efficient and numpy-styled

--- Hdf5/PythonSamples/add_array.py
+++ Hdf5/PythonSamples/add_array.py
@@ -6,9 +6,10 @@ import tables
 
 def create_array(x_dim, y_dim):
     f = lambda x, y: x**2 + 2*x*y
-    a = np.array([[f(x/float(x_dim), y/float(y_dim))
-                       for y in xrange(y_dim)] for x in xrange(x_dim)])
-    return a
+    x = np.linspace(0.0, 1.0, x_dim, endpoint=False)
+    y = np.linspace(0.0, 1.0, y_dim, endpoint=False)
+    X, Y = np.meshgrid(x, y, indexing='ij')
+    return f(X, Y)
 
 def add_array(name):
     with tables.openFile(name, mode='a') as h5file:
