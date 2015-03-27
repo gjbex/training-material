cf2538fc5d99d9dc89d0d822b3ca994bd8aca944
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-03-27 07:10:04 +0100

    Fixed silly naming of variable

--- Fortran/ControlStructures/select_test.f90
+++ Fortran/ControlStructures/select_test.f90
@@ -2,7 +2,7 @@ program select_test
 implicit none
 integer :: a, b
 character(len=40) :: buffer
-character(len=1) :: operand
+character(len=1) :: operator
 
 if (command_argument_count() /= 3) then
     print *, '# error: 3 arguments expected'
@@ -12,17 +12,17 @@ end if
 call get_command_argument(1, buffer)
 read (buffer, '(I3)') a
 call get_command_argument(2, buffer)
-operand = trim(buffer)
+operator = trim(buffer)
 call get_command_argument(3, buffer)
 read (buffer, '(I3)') b
 
-select case (operand)
+select case (operator)
     case ('+')
-        print "(I3, A2, I3, ' = ', I6)", a, operand, b, a + b
+        print "(I3, A2, I3, ' = ', I6)", a, operator, b, a + b
     case ('*')
-        print "(I3, A2, I3, ' = ', I6)", a, operand, b, a * b
+        print "(I3, A2, I3, ' = ', I6)", a, operator, b, a * b
     case default
-        print "('# error: unknown operand ''', A, '''')", operand
+        print "('# error: unknown operand ''', A, '''')", operator
 end select
         
 end program select_test
