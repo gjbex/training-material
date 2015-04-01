20fc56f9ff7feb20ba9b9a1a64c310c5161bdd10
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-04-01 16:24:15 +0200

    Simplified code to use assumed size array passing of label

--- Fortran/Matrices/pointer_test.f90
+++ Fortran/Matrices/pointer_test.f90
@@ -1,17 +1,15 @@
 program pointer_test
-    use iso_fortran_env
+    use, intrinsic :: iso_fortran_env
     use matrix_ops
     implicit none
     integer, parameter :: m = 5, n = 4
     real(kind=REAL64), dimension(:, :), allocatable, target :: a
     real(kind=REAL64), dimension(:, :), pointer :: p
-    character(len=:), allocatable :: label
 
     allocate(A(m, n))
     p => A
     call init_matrix(p)
-    label = 'A'
-    call print_matrix(p, label)
+    call print_matrix(p, 'A')
     deallocate(A)
 
 end program pointer_test
