05cc542336e1e4b8dcb4ce848732d80330562cca
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-04-17 13:16:40 +0200

    Added example of complex type declarationss

---
+++ Fortran/Types/complex_test.f90
@@ -0,0 +1,10 @@
+program complex_test
+    use, intrinsic :: iso_fortran_env
+    implicit none
+    complex :: z_default
+    complex(kind=real32) :: z_32
+    complex(kind=real64) :: z_64
+
+    print *, kind(z_default), kind(z_32), kind(z_64)
+
+end program complex_test
