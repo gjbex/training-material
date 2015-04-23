d3e8c1dd51c674e2a3ca88d38a1692011d30a8be
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-04-23 06:56:03 +0200

    Added example of statement separation by ;

--- Fortran/Miscellaneous/free_source_form_test.f90
+++ Fortran/Miscellaneous/free_source_form_test.f90
@@ -1,11 +1,14 @@
 PROGRAM free_source_form_test
     INTEGER :: i
     INTEGER, PARAMETER :: n = 10
+    REAL :: x, y, z
 
     ! This will print something to screen
     DO I = 1, n
         print '(A, I2, A, I2)', 'This is iteration ', I,    &
                                  ' out of ', n
     END DO
+    x = 3; y = 5; z = 7
+    print *, x, y, z
 
 END PROGRAM free_source_form_test
