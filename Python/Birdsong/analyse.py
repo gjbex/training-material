accd73a3ce45babb6257856879f6d26614e4fec7
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-04-27 18:59:29 +0200

    Remoded FFT based filtering code

--- Python/Birdsong/analyse.py
+++ Python/Birdsong/analyse.py
@@ -13,29 +13,14 @@ def compute_spectrum(signal, rate):
     Y = sp.fft(signal)/n
     return freq, Y
 
-def filter_spectrum(frq, Y, level=0.001, window=5000):
-    Y_filtered = np.array(Y)
-    Y_filtered[np.abs(Y) < level] = 0.0
-    half = len(Y_filtered)/2
-    Y_filtered[half - window:half + window] = 0.0
-    return Y_filtered
-
-def compute_signal(freq, Y):
-    y = (len(Y)*np.real(sp.ifft(Y))).astype(np.uint8)
-    return y
-
 if __name__ == '__main__':
     from argparse import ArgumentParser
     import sys
     import sound_plot
     arg_parser = ArgumentParser(description='analyse WAV file')
     arg_parser.add_argument('wav_file', help='WAV file to analyse')
-    arg_parser.add_argument('--filter', action='store_true',
-                            help='perform filter operations')
-    arg_parser.add_argument('--filtered_wav_file',
-                            help='WAV file to write the filtered signal')
     options = arg_parser.parse_args()
-    nr_cols = 2 if options.filter else 1
+    nr_cols = 1
 
     rate, signal = wav_io.read(options.wav_file)
     n = len(signal)
@@ -46,16 +31,4 @@ if __name__ == '__main__':
     sound_plot.plot_signal(time, signal)
     plt.subplot(2, nr_cols, 2)
     sound_plot.plot_spectrum(freq, Y, rate)
-
-    if options.filter:
-        Y_filtered = filter_spectrum(freq, Y, level=0.01, window=10000)
-        signal_filtered = compute_signal(freq, Y_filtered)
-        if options.filtered_wav_file:
-            wav_io.write(options.filtered_wav_file, rate, signal_filtered)
-# plot filtered signal and spectrum
-        plt.subplot(2, nr_cols, 4)
-        sound_plot.plot_spectrum(freq, Y_filtered, rate)
-        plt.subplot(2, nr_cols, 3)
-        sound_plot.plot_signal(time, signal_filtered)
-
     plt.show()
