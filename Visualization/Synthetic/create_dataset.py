6a0e6c73bad3c0d2ba06e86c227f1e2673650d4f
gjbex <geertjan.bex@uhasselt.be>
2015-05-06 15:43:09 +0200

    Added generation of vector field; replace - by _ in command line option names; made command line options more consistent; added option not to include grid coordinates

--- Visualization/Synthetic/create_dataset.py
+++ Visualization/Synthetic/create_dataset.py
@@ -35,24 +35,26 @@ def compute_particles(hrfile, center, nr_particles=10, avg_dist=10.0,
     h5file.flush()
 
 
-def compute_grid(h5file, group='grid', extent=750.0, points=1000):
+def compute_grid(h5file, group='grid', extent=750.0, points=1000,
+                 add_grid=True):
     '''compute grid for scalar and vector field data'''
     x = np.linspace(-extent, extent, points, endpoint=True)
     y = np.linspace(-extent, extent, points, endpoint=True)
     z = np.linspace(-extent, extent, points, endpoint=True)
-    grid = h5file.createGroup(h5file.root, group, 'grid coordinates')
-    h5file.create_array(grid, 'x', x, 'x coordinates')
-    h5file.create_array(grid, 'y', y, 'y coordinates')
-    h5file.create_array(grid, 'z', z, 'z coordinates')
-    h5file.flush()
+    if add_grid:
+        grid = h5file.createGroup(h5file.root, group, 'grid coordinates')
+        h5file.create_array(grid, 'x', x, 'x coordinates')
+        h5file.create_array(grid, 'y', y, 'y coordinates')
+        h5file.create_array(grid, 'z', z, 'z coordinates')
+        h5file.flush()
     return x, y, z
 
 def compute_scalar_field(h5file, centers, xs, ys, zs, max_field=5000.0,
                          array='scalar'):
-    '''compute scalar field, that decreases quadratically with the
+    '''compute scalar field, that decreases linearly with the
        distance to the center'''
     field = h5file.create_earray(h5file.root, array, tables.Float64Atom(),
-                         (len(xs), len(ys), 0), 'scalar field data')
+                                 (len(xs), len(ys), 0), 'scalar field data')
     for z in zs:
         X, Y, Z = np.meshgrid(xs, ys, np.array(z))
         field_slice = np.zeros((len(xs), len(ys), 1))
@@ -63,34 +65,61 @@ def compute_scalar_field(h5file, centers, xs, ys, zs, max_field=5000.0,
         field.append(field_slice)
     h5file.flush()
 
+def compute_vector_field(h5file, centers, xs, ys, zs, max_field=50.0,
+                         group='vector'):
+    '''compute a vector field that decreases quadratically with the
+       distance to the center'''
+    vector = h5file.createGroup(h5file.root, group)
+    dims = ['{0}_{1}'.format(group, dim) for dim in ['x', 'y', 'z']]
+    for i, dim in enumerate(dims):
+        comp = h5file.create_earray(vector, dim, tables.Float64Atom(),
+                                    (len(xs), len(ys), 0),
+                                    'field {0}-component'.format(dim))
+        for z in zs:
+            coords = np.meshgrid(xs, ys, np.array(z))
+            field_slice = np.zeros((len(xs), len(ys), 1))
+            for center in centers:
+                dist = np.sqrt((coords[0] - center[0])**2 +
+                               (coords[1] - center[1])**2 +
+                               (coords[2] - center[2])**2 + 1.0)
+                field_slice += max_field*(coords[i] - center[i])/dist**2
+            comp.append(field_slice)
+        h5file.flush()
+
 if __name__ == '__main__':
     from argparse import ArgumentParser
     import sys
     arg_parser = ArgumentParser(description='data generator')
     arg_parser.add_argument('--centers', type=int, default=5,
                             help='number of centers')
-    arg_parser.add_argument('--particle-data', action='store_true',
+    arg_parser.add_argument('--particle_data', action='store_true',
                             help='generate particle data')
-    arg_parser.add_argument('--avg-center-dist', type=float, default=100.0,
+    arg_parser.add_argument('--avg_center_dist', type=float, default=100.0,
                             help='average distance between centers')
     arg_parser.add_argument('--particles', type=int, default=10,
                             help='number of particles/center')
-    arg_parser.add_argument('--avg-part-dist', type=float, default=5.0,
+    arg_parser.add_argument('--avg_part_dist', type=float, default=5.0,
                             help='average distance between particles')
-    arg_parser.add_argument('--avg-part-radial-vel', type=float,
+    arg_parser.add_argument('--avg_part_radial-vel', type=float,
                             default=1.0,
                             help='average radial velocity of particles')
-    arg_parser.add_argument('--avg-part-random-vel', type=float,
+    arg_parser.add_argument('--avg_part_random-vel', type=float,
                             default=0.1,
                             help='average random velocity of particles')
-    arg_parser.add_argument('--scalar-field-data', action='store_true',
-                            help='generate scalar field data')
-    arg_parser.add_argument('--points', type=int, default=1000,
+    arg_parser.add_argument('--no_grid_data', action='store_true',
+                            help='do not include grid points')
+    arg_parser.add_argument('--points', type=int, default=100,
                             help='number of points in each dimension')
     arg_parser.add_argument('--extent', type=float, default=750.0,
                             help='maxumum grid coordinate')
-    arg_parser.add_argument('--max_temp', type=float, default=5000.0,
-                            help='maximum temperature')
+    arg_parser.add_argument('--scalar_field_data', action='store_true',
+                            help='generate scalar field data')
+    arg_parser.add_argument('--max_scalar', type=float, default=5000.0,
+                            help='maximum scalar field value')
+    arg_parser.add_argument('--vector_field_data', action='store_true',
+                            help='generate vector field data')
+    arg_parser.add_argument('--max_vector', type=float, default=5000.0,
+                            help='maximum vector field norm')
     arg_parser.add_argument('--verbose', action='store_true',
                             help='verbose output for debugging')
     arg_parser.add_argument('file', help='HDF5 file to store the data in')
@@ -107,7 +136,8 @@ if __name__ == '__main__':
                           options.avg_part_radial_vel,
                           options.avg_part_random_vel)
     x, y, z = compute_grid(h5file, extent=options.extent,
-                           points=options.points)
+                           points=options.points,
+                           add_grid=not options.no_grid_data)
     if options.verbose:
         print 'grid x'
         print x
@@ -116,5 +146,7 @@ if __name__ == '__main__':
         print 'grid z'
         print z
     if options.scalar_field_data:
-        compute_scalar_field(h5file, centers, x, y, z, options.max_temp)
+        compute_scalar_field(h5file, centers, x, y, z, options.max_scalar)
+    if options.vector_field_data:
+        compute_vector_field(h5file, centers, x, y, z, options.max_vector)
     h5file.close()
