c7439d56b42eb2ef757e6fc74d719e47bedfa124
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-05-11 15:59:38 +0200

    Added extra local variable to block construct

--- Fortran/ControlStructures/block_test.f90
+++ Fortran/ControlStructures/block_test.f90
@@ -5,8 +5,11 @@ program block_test
     do i = 1, 10
         block
             integer :: j
+            real :: frac
             j = i*3
             print *, i, j
+            frac = 1.0/i
+            print *, i, frac
         end block
     end do
 end program block_test
