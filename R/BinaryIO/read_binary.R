0fa751664f99ee53c5c1d659bcbce1a0138c2aaa
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-05-18 17:45:40 +0200

    Improved code style, output format; added input file and associated index

--- R/BinaryIO/read_binary.R
+++ R/BinaryIO/read_binary.R
@@ -1,5 +1,12 @@
-conn <- file('data.txt', 'rb')
-current.pos <- seek(conn, where = 15, origin = 'start')
-data <- readBin(conn, 'raw', n = 5)
+#!/usr/bin/env Rscript
+
+args <- commandArgs(TRUE)
+file_name <- args[1]
+file_pos <- as.integer(args[2])
+line_length <- as.integer(args[3])
+
+conn <- file(file_name, 'rb')
+current.pos <- seek(conn, where = file_pos, origin = 'start')
+data <- readBin(conn, 'raw', n = line_length)
 s <- paste(sapply(data, rawToChar), collapse='')
-s
+cat(paste('|', s, '|', sep=''), sep='')
