142b4564bf2faa51e47f1199cf71a3f0887de79f
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-05-18 16:46:19 +0200

    Added a line indexer/tester as example for binary read/write, CSV write

---
+++ Python/DataFormats/line_indexer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser, FileType
+    import csv, sys
+    arg_parser = ArgumentParser(description='create a CSV file with start '
+                                            'line positions and line '
+                                            'lengths for a given file')
+    arg_parser.add_argument('input_file', type=FileType('rb'),
+                            help='file to index')
+    arg_parser.add_argument('--output_file', type=FileType('w'),
+                            help='CSV output file')
+    arg_parser.add_argument('--no_header', action='store_true',
+                            help='do not add header row to CSV file')
+    options = arg_parser.parse_args()
+    if options.output_file:
+        csv_writer = csv.writer(options.output_file)
+    else:
+        csv_writer = csv.writer(sys.stdout)
+    if not options.no_header:
+        csv_writer.writerow(['position', 'line_length'])
+    c = options.input_file.read(1)
+    length = 0
+    pos = 0
+    line_pos = 0
+    while c:
+        pos += 1
+        if c == '\r':
+            pass
+        elif c == '\n':
+            csv_writer.writerow([line_pos, length])
+            length = 0
+            line_pos = pos
+        else:
+            length += 1
+        c = options.input_file.read(1)
---
+++ Python/DataFormats/read_line_index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser, FileType
+    arg_parser = ArgumentParser(description='read an index line')
+    arg_parser.add_argument('file', type=FileType('rb'),
+                            help='file to read')
+    arg_parser.add_argument('--pos', type=int, required=True,
+                            help='start position of line')
+    arg_parser.add_argument('--len', type=int, required=True,
+                            help='line length')
+    options = arg_parser.parse_args()
+    options.file.seek(options.pos)
+    line = options.file.read(options.len)
+    print '|{0}|'.format(line)
