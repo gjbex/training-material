bccd78b2408e2bb5fcc048cba41da59aff361635
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-05-21 08:36:31 +0200

    improved error handling; code formatting; output formatting

--- Fortran/ControlStructures/select_test.f90
+++ Fortran/ControlStructures/select_test.f90
@@ -1,28 +1,32 @@
 program select_test
-implicit none
-integer :: a, b
-character(len=40) :: buffer
-character(len=1) :: operator
+    use :: iso_fortran_env, only : error_unit
+    implicit none
+    integer :: a, b
+    character(len=40) :: buffer
+    character(len=1) :: operator
 
-if (command_argument_count() /= 3) then
-    print *, '# error: 3 arguments expected'
-    stop
-end if
+    if (command_argument_count() /= 3) then
+        write (unit=error_unit, fmt='(A)') &
+                '# error: 3 arguments expected'
+        stop 1
+    end if
 
-call get_command_argument(1, buffer)
-read (buffer, '(I3)') a
-call get_command_argument(2, buffer)
-operator = trim(buffer)
-call get_command_argument(3, buffer)
-read (buffer, '(I3)') b
+    call get_command_argument(1, buffer)
+    read (buffer, '(I3)') a
+    call get_command_argument(2, buffer)
+    operator = trim(buffer)
+    call get_command_argument(3, buffer)
+    read (buffer, '(I3)') b
 
-select case (operator)
-    case ('+')
-        print "(I3, A2, I3, ' = ', I6)", a, operator, b, a + b
-    case ('*')
-        print "(I3, A2, I3, ' = ', I6)", a, operator, b, a * b
-    case default
-        print "('# error: unknown operand ''', A, '''')", operator
-end select
-        
+    select case (operator)
+        case ('+')
+            print "(I0, ' ', A, ' ', I0, ' = ', I0)", a, operator, b, a + b
+        case ('*')
+            print "(I0, ' ', A, ' ', I0, ' = ', I0)", a, operator, b, a * b
+        case default
+            write (unit=error_unit, fmt='(3A)')  &
+                    '# error: unknown operand ''', operator, ''''
+            stop 2
+    end select
+            
 end program select_test
