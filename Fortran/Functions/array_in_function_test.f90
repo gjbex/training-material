4483e2eebd371792bc4536e7af7106bb29e5db71
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-05-21 08:56:43 +0200

    Simplified code by using elemental random_number; eleminated superfluous variables

--- Fortran/Functions/array_in_function_test.f90
+++ Fortran/Functions/array_in_function_test.f90
@@ -1,27 +1,19 @@
 program static_array_in_function_test
-    use, intrinsic :: iso_fortran_env
     implicit none
     integer, parameter :: n = 100
-    real(kind=real32) :: result
 
-    result = compute_normal_avg(n)
-    print *, result
+    print '(F10.5)', compute_normal_avg(n)
 
 contains
 
     function compute_normal_avg(n) result(average)
+        use, intrinsic :: iso_fortran_env, only : REAL32
         implicit none
         integer, intent(in) :: n
         real(kind=REAL32) :: average
         real(kind=REAL32), dimension(n) :: v
-        real(kind=REAL32) :: random
-        integer :: i
 
-        do i = 1, n
-            call random_number(random)
-            v(i) = random
-        end do
-
+        call random_number(v)
         average = sum(v)/n
 
     end function compute_normal_avg
