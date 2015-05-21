51ccfdb112ba538ed83804b4b2ec305ffd814cd6
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-05-21 17:06:53 +0200

    Improved matrix initialization; added use only

--- Fortran/Matrices/subarray_test.f90
+++ Fortran/Matrices/subarray_test.f90
@@ -1,18 +1,16 @@
 program subarray_test
-    use, intrinsic :: iso_fortran_env
-    use matrix_ops
+    use, intrinsic :: iso_fortran_env, only : dp => REAL64
+    use :: matrix_ops, only : print_matrix, print_vector
     implicit none
-    integer, parameter :: m = 4, n = 5, dp = REAL64
+    integer, parameter :: m = 4, n = 5
     real(kind=dp), dimension(m, n) :: a
     real(kind=dp), dimension(m/2, n/2) :: b
     real(kind=dp), dimension(m -2) :: v
     integer :: i, j
 
-    do j = 1, n
-        do i = 1, m
-            a(i, j) = j + (i - 1)*n
-        end do
-    end do
+    forall (i = 1:n, j = 1:n)
+        a(i, j) = j + (i - 1)*n
+    end forall
     call print_matrix(a, 'A')
     call print_vector(a(2, :), 'row 2 of A')
     call print_vector(a(:, 3), 'column 3 of A')
