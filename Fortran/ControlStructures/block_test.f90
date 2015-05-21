a5afb107c85226379f015f5810e3e0f2b1391fec
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-05-21 08:15:52 +0200

    Improved example of block local variables

--- Fortran/ControlStructures/block_test.f90
+++ Fortran/ControlStructures/block_test.f90
@@ -1,15 +1,18 @@
 program block_test
     implicit none
-    integer :: i
+    integer :: i, j
 
+    j = -10
+    print '(A, I0)', 'global j: ', j
     do i = 1, 10
         block
             integer :: j
             real :: frac
             j = i*3
-            print *, i, j
+            print '(A, I0, A, I0)', 'local j for ', i, ' = ', j
             frac = 1.0/i
             print *, i, frac
         end block
     end do
+    print '(A, I0)', 'global j: ', j
 end program block_test
