2ab8c8e6271a1eeb02545fecf500990d760bdfb3
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-05-22 10:42:43 +0200

    Added use only; formatted code

--- Fortran/Types/rand_test.f90
+++ Fortran/Types/rand_test.f90
@@ -1,15 +1,14 @@
 program rand_test
-use, intrinsic :: iso_fortran_env
-use random_mod
-implicit none
-integer, parameter :: sp = REAL32, dp = REAL64, N = 10000
-real(kind=dp) :: r
-integer :: i
+    use, intrinsic :: iso_fortran_env, only : sp => REAL32, dp => REAL64
+    use random_mod, only : random_normal_number
+    implicit none
+    integer, parameter :: N = 10000
+    real(kind=dp) :: r
+    integer :: i
 
-do i = 1, N
-    call random_normal_number(r)
-    print *, r
-end do
+    do i = 1, N
+        call random_normal_number(r)
+        print *, r
+    end do
 
 end program rand_test
-
