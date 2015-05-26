725e1386bd501c7831ceb2b14a306dc22767624e
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-05-26 13:16:27 +0200

    Added general Gauss-Legendre quadrature logic to original Gauss module to enable usage of private procedures in derived types

--- Fortran/Integrate/xw_test.f90
+++ Fortran/Integrate/xw_test.f90
@@ -1,10 +1,20 @@
 program xw_test
     use, intrinsic :: iso_fortran_env, only : dp => REAL64
-    use :: gen_gauss_mod
+    use :: gauss_mod
     implicit none
     type(gen_gauss_type) :: gen_gauss_quad
-    integer, parameter :: n = 100
-    call gen_gauss_quad%init(n, precision=1.0e-15_dp)
+    integer, parameter :: default_n = 10
+    integer :: n
+    character(len=20) :: buffer
+
+    if (command_argument_count() >= 1) then
+        call get_command_argument(1, buffer)
+        read (buffer, '(I10)') n
+    else
+        n = default_n
+    end if
+
+    call gen_gauss_quad%init_gen_gauss(n, precision=1.0e-15_dp)
     print '(2A20)', 'abscis', 'weight'
     call gen_gauss_quad%print_params()
 
