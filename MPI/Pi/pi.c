8fe4a39886da75f85a17a3946f0f9041b525d424
gjbex <geertjan.bex@uhasselt.be>
2015-06-04 15:12:43 +0200

    Fixed seeding bug

--- MPI/Pi/pi.c
+++ MPI/Pi/pi.c
@@ -3,8 +3,10 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <mpi.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 
-int random_seed(int rank);
 double compute_pi(long n, int seed);
 
 int main(int argc, char *argv[]) {
@@ -14,7 +16,6 @@ int main(int argc, char *argv[]) {
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    seed = random_seed(rank);
     if (rank == 0) {
         if (argc < 2) {
             n = 1000;
@@ -24,7 +25,7 @@ int main(int argc, char *argv[]) {
         n /= size;
     }
     MPI_Bcast(&n, 1, MPI_LONG, 0, MPI_COMM_WORLD);
-    pi = compute_pi(n, seed);
+    pi = compute_pi(n, rank);
     printf("rank %d: %ld, %.5f\n", rank, n, pi);
     MPI_Reduce(&pi, &global_pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     if (rank == 0) {
@@ -34,21 +35,28 @@ int main(int argc, char *argv[]) {
     return EXIT_SUCCESS;
 }
 
-int random_seed(int rank) {
-    struct timeval time;
-    gettimeofday(&time, NULL);
-    return (int) (time.tv_usec*(rank + 1) + time.tv_sec/(rank + 1));
-}
-
-double compute_pi(long n, int seed) {
-    long i;
-    double count = 0.0, x, y;
-#pragma omp parallel for private(x, y) reduction(+:count)
-    for (i = 0; i < n; i++) {
-        x = ((double) rand_r(&seed))/RAND_MAX;
-        y = ((double) rand_r(&seed))/RAND_MAX;
-        if (x*x + y*y <= 1.0) {
-            count += 1.0;
+double compute_pi(long n, int rank) {
+    double count = 0.0;
+#pragma omp parallel shared(count, n, rank) default(none)
+    {
+        long i;
+        struct timeval time;
+        gettimeofday(&time, NULL);
+        int seed = (int) (time.tv_usec*(17*rank + 1) +
+                              time.tv_sec/(rank + 1));
+#ifdef _OPENMP
+        int nr_threads = 1, thread_nr = 0;
+        nr_threads = omp_get_num_threads();
+        thread_nr = omp_get_thread_num();
+        seed += 17*thread_nr;
+#endif
+#pragma omp for reduction(+:count)
+        for (i = 0; i < n; i++) {
+            double x = ((double) rand_r(&seed))/RAND_MAX;
+            double y = ((double) rand_r(&seed))/RAND_MAX;
+            if (x*x + y*y <= 1.0) {
+                count += 1.0;
+            }
         }
     }
     return 4.0*count/n;
