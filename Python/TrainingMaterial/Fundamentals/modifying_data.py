e9a9f410397980ecf48c970cfffc6ba7c7aa11a5
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-08-11 20:39:14 +0200

    Updated for py3k

--- Python/TrainingMaterial/Fundamentals/modifying_data.py
+++ Python/TrainingMaterial/Fundamentals/modifying_data.py
@@ -2,14 +2,14 @@
 
 import sys
 
+
 def main():
-    print sys.stdin.readline().rstrip('\r\n')
+    print(sys.stdin.readline().rstrip('\r\n'))
     for line in sys.stdin:
         data = line.rstrip('\r\n').split()
         if float(data[2]) < 0.0:
             data[2] = 0.0
-        print data[0], data[1], data[2]
+        print(data[0], data[1], data[2])
 
 if __name__ == '__main__':
     main()
-
