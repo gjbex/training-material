fafd114cc97307b8121b8a24e9ee062e1af7df74
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-08-13 09:26:06 +0200

    Migrated to py3k

--- Python/Exercises/ConvertingTime/time2seconds.py
+++ Python/Exercises/ConvertingTime/time2seconds.py
@@ -2,15 +2,15 @@
 
 import sys
 
+
 def main():
     units = [1, 60, 3600]
     parts = [int(x) for x in sys.argv[1].split(':')]
     parts.reverse()
     result = sum([x*y for x, y in zip(parts, units)])
-    print result
+    print(result)
     return 0
 
 if __name__ == '__main__':
     status = main()
     sys.exit(status)
-
