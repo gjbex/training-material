97576e1bdecbe8cb36017c500e9f85aa73589556
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-08-13 09:43:30 +0200

    Migrated to py3k

--- Python/Exercises/Durations/durations.py
+++ Python/Exercises/Durations/durations.py
@@ -3,10 +3,12 @@
 from datetime import datetime
 import sys
 
+
 def parse_line(line):
     evt, evt_time = (x.strip() for x in line.split(':', 1))
     evt_type, evt_id = evt.split()
     return evt_type, evt_id, evt_time
+
 
 def main():
     status = 0
@@ -17,21 +19,22 @@ def main():
         evt_type, evt_id, evt_time = parse_line(line)
         if evt_type == 'start':
             if evt_id in evt_starts:
-                sys.stderr.write('### error: multiple starts for {0}\n'.format(evt_id))
+                msg = '### error: multiple starts for {0}\n'
+                sys.stderr.write(msg.format(evt_id))
                 status = 1
             evt_starts[evt_id] = datetime.strptime(evt_time,
                                                    '%Y-%m-%d %H:%M:%S')
         elif evt_type == 'end':
             if evt_id not in evt_starts:
-                sys.stderr.write('### error: end without start for {0}\n'.format(evt_id))
+                msg = '### error: end without start for {0}\n'
+                sys.stderr.write(msg.format(evt_id))
                 status = 2
             else:
-                duration = datetime.strptime(evt_time,
-                                             '%Y-%m-%d %H:%M:%S') - evt_starts[evt_id]
-                print '{0}: {1}'.format(evt_id, str(duration))
+                end_time = datetime.strptime(evt_time, '%Y-%m-%d %H:%M:%S')
+                duration = end_time - evt_starts[evt_id]
+                print('{0}: {1}'.format(evt_id, str(duration)))
     return status
 
 if __name__ == '__main__':
     status = main()
     sys.exit(status)
-
