991fd990127e464ed756618262a9bc04f2c59d3d
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-08-14 21:29:42 +0200

    Migrated to py3k

--- Python/DataFormats/read_commented_csv.py
+++ Python/DataFormats/read_commented_csv.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python
 
-import csv, re, sys
+import csv
+import re
+import sys
 
 if __name__ == '__main__':
     file_name = sys.argv[1]
-    with open(file_name, 'rb') as csv_file:
-        dialect = csv.Sniffer().sniff(csv_file.read(1024))
-        regex_str = r'^\s*#\s+(.+?){0}+$'.format(dialect.delimiter)
-    with open(file_name, 'r') as csv_file:
+    regex_str = r'^\s*#\s*(.+?)$'
+    with open(file_name, 'r', newline='') as csv_file:
         header_bytes = 0
         for line in csv_file:
-            if line.startswith('#'):
+            if line.lstrip().startswith('#'):
                 header_bytes += len(line)
                 line = re.sub(regex_str, r'\1', line.strip('\r\n'))
-                print 'comment: "{0}"'.format(line)
+                print('comment: "{0}"'.format(line))
             else:
                 break
-    with open(file_name, 'rb') as csv_file:
+    with open(file_name, 'r') as csv_file:
+        csv_file.seek(header_bytes)
+        dialect = csv.Sniffer().sniff(csv_file.read(1024))
         csv_file.seek(header_bytes)
         csv_reader = csv.DictReader(csv_file, fieldnames=None,
                                     restkey='rest', restval=None,
                                     dialect=dialect)
         try:
-            row = csv_reader.next()
-            print 'headers: {0}'.format(':'.join(csv_reader.fieldnames))
+            row = next(csv_reader)
+            print('headers: {0}'.format(':'.join(csv_reader.fieldnames)))
             while row:
-                print row
-                row = csv_reader.next()
+                print(row)
+                row = next(csv_reader)
         except StopIteration:
             pass
-
--- Python/DataFormats/read_csv.py
+++ Python/DataFormats/read_csv.py
@@ -3,25 +3,25 @@
 import csv
 from argparse import ArgumentParser
 
+
 def main():
     arg_parser = ArgumentParser(description='reads csv file, summarize')
     arg_parser.add_argument('--file', type=str, action='store',
                             dest='file', help='CSV file to parse')
     options = arg_parser.parse_args()
-    with open(options.file, 'rb') as csv_file:
+    with open(options.file, 'r') as csv_file:
         dialect = csv.Sniffer().sniff(csv_file.read(1024))
         csv_file.seek(0)
         sum = 0.0
         csv_reader = csv.DictReader(csv_file, fieldnames=None,
                                     restkey='rest', restval=None,
                                     dialect=dialect)
-        print csv_reader.fieldnames
+        print('headers: {0}'.format(':'.join(csv_reader.fieldnames)))
         for row in csv_reader:
-            print '{name} --- {weight}'.format(name=row['name'],
-                                               weight=row['weight'])
+            print('{name} --- {weight}'.format(name=row['name'],
+                                               weight=row['weight']))
             sum += float(row['weight'])
-        print 'sum = {}'.format(sum)
+        print('sum = {}'.format(sum))
 
 if __name__ == '__main__':
     main()
-    
