a9c136cff33d8127ed27ae98cb08fa2d6874e3ef
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-08-17 00:51:07 +0200

    Migraged to py3k; 2 is now also prime

--- Python/Iterators/primes_itertools.py
+++ Python/Iterators/primes_itertools.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
 
-import itertools, math, sys
+import itertools
+import math
+import sys
+
 
 def is_prime(n):
     '''returns True if n is prime, False otherwise'''
-    if n <= 2:
+    if n < 2:
         return False
-    for i in xrange(2, 1 + int(math.sqrt(n))):
+    elif n == 2:
+        return True
+    for i in range(2, 1 + int(math.sqrt(n))):
         if n % i == 0:
             return False
     return True
 
+
 def main():
-    for n in itertools.ifilter(is_prime, itertools.count()):
-        print n
+    for n in filter(is_prime, itertools.count()):
+        print(n)
     return 0
 
 if __name__ == '__main__':
     status = main()
     sys.exit(status)
-
