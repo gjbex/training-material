cb25bc81a3782fd2bb9fe0e42435ef1b73e8f306
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-10-01 08:27:51 +0200

    Added functions to deal with HDF5 file

--- Math/Lapack/SVD/svd_blacs.f90
+++ Math/Lapack/SVD/svd_blacs.f90
@@ -1,5 +1,5 @@
 program svd_blacs
-    use, intrinsic :: iso_fortran_env, only : dp => REAL64
+    use, intrinsic :: iso_fortran_env, only : dp => REAL64, error_unit
     use matrix_mod, only : get_matrix_dimensions
     use utils_mod, only : get_file_name, get_dataset_name
     implicit none
@@ -15,7 +15,7 @@ program svd_blacs
     integer, dimension(2) :: matrix_dims
     character, parameter :: orientation = "R"
     integer :: numroc
-    real(kind=dp), dimension(:, :), allocatable :: A
+    real(kind=dp), dimension(:, :), allocatable :: A_local
     integer :: i
 
 ! initialize BLACS (initializes MPI under the hood), and determine
@@ -39,7 +39,7 @@ program svd_blacs
                             " x ", matrix_dims(2)
     end if
 
-! determine size of local storage
+! determine size of local storage and allocate
     call compute_local_storage_sizes(context, &
                                      nr_matrix_rows, nr_matrix_cols, &
                                      row_block_size, col_block_size, &
@@ -50,6 +50,13 @@ program svd_blacs
                             ", proc col ", proc_col, &
                             ", local rows ", nr_local_rows, &
                             ", local cols ", nr_local_cols
+    end if
+    allocate(A_local(nr_local_rows, nr_local_cols),stat=ierr)
+    if (ierr /= 0) then
+        write (unit=error_unit, fmt="(A, I0, A, I0, A, I0)")  &
+            "proc_nr ", proc_nr, " can not allocate ", &
+            nr_local_rows, " x ", nr_local_cols
+        call blacs_exit(0)
     end if
 
     row_start = proc_row*row_block_size
@@ -69,6 +76,9 @@ program svd_blacs
             end if
         end do
     end do
+
+! deallocate memory
+    deallocate(A_local)
 
 ! finalize BLACS (finalizes MPI as well)
     call blacs_exit(0)
@@ -116,4 +126,66 @@ contains
         local_cols = numroc(nr_matrix_cols, col_block_size, proc_col, 0, &
                            nr_proc_cols)
     end subroutine compute_local_storage_sizes
+    
+    subroutine init_read(file_name, file_id)
+        use hdf5
+        use, intrinsic :: iso_fortran_env, only : error_unit
+        implicit none
+        character(len=*), intent(in) :: file_name
+        integer(hid_t), intent(out) :: file_id
+        integer :: error
+        ! initialize Fortran interface
+        call h5open_f(error)
+
+        ! open the HDF5 file, overwriting it if it alread exists
+        call h5fopen_f(file_name, H5F_ACC_RDONLY_F, file_id, error)
+        if (error /= 0) then
+            write (unit=error_unit, fmt="(2A)") "# error opening file ", &
+                                                file_name
+            call blacs_exit(0)
+        end if
+    end subroutine init_read
+
+    subroutine init_dataset(file_id, matrix_dset_name, matrix_dset_id, &
+                            matrix_fspace_id, matrix_dims)
+        use hdf5
+        use, intrinsic :: iso_fortran_env, only : error_unit
+        implicit none
+        integer(hid_t), intent(in) :: file_id
+        character(len=*), intent(in) :: matrix_dset_name
+        integer(hid_t), intent(out) :: matrix_fspace_id, matrix_dset_id
+        integer(hsize_t), dimension(2), intent(out) :: matrix_dims
+        integer(hsize_t), dimension(2) :: max_matrix_dims
+        integer :: error
+        ! open the matrix dataset, and get the matrix file dataspace
+        call h5dopen_f(file_id, matrix_dset_name, matrix_dset_id, error)
+        call h5dget_space_f(matrix_dset_id, matrix_fspace_id, error)
+
+        ! retrieve the dimensions of the matrix, and allocate the matrix
+        call h5sget_simple_extent_dims_f(matrix_fspace_id, matrix_dims, &
+                                         max_matrix_dims, error)
+    end subroutine init_dataset
+
+    subroutine finalize_dataset(matrix_dset_id, matrix_fspace_id)
+        use hdf5
+        implicit none
+        integer(hid_t), intent(inout) :: matrix_fspace_id, matrix_dset_id
+        integer :: error
+        ! close dataspaces
+        call h5sclose_f(matrix_fspace_id, error)
+        ! close the matrix dataset
+        call h5dclose_f(matrix_dset_id, error)
+    end subroutine finalize_dataset
+
+    subroutine finalize_read(file_id)
+        use hdf5
+        implicit none
+        integer(hid_t), intent(inout) :: file_id
+        integer :: error
+        ! close HDF5 file
+        call h5fclose_f(file_id, error)
+        ! finalize Fortran HDF5 interface
+        call h5close_f(error)
+    end subroutine finalize_read
+
 end program svd_blacs
