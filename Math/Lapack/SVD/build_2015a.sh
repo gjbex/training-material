04935e88d14400191cc92a57c7e2e9152bfb37b1
gjbex <geertjan.bex@uhasselt.be>
2015-10-08 13:53:07 +0200

    Using lp64 rather than ilp64 matrix addressing

--- Math/Lapack/SVD/build_2015a.sh
+++ Math/Lapack/SVD/build_2015a.sh
@@ -6,7 +6,7 @@ module load intel/2015a
 module load HDF5/1.8.14-intel-2015a-serial
 
 FFLAGS="-O2 -g -openmp -I${MKLROOT}/include"
-MATH_LIBS=" -L${MKLROOT}/lib/intel64 -lmkl_intel_ilp64 -lmkl_core -lmkl_sequential -lpthread -lm"
+MATH_LIBS=" -L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lpthread -lm"
 
 DISTR_MATH_LIBS=" -L${MKLROOT}/lib/intel64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lmkl_blacs_intelmpi_lp64 -lpthread -lm"
 
