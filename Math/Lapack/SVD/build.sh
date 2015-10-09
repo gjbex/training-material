59bb257692f14116f83ec4aec0706a36adfc5832
gjbex <geertjan.bex@uhasselt.be>
2015-10-09 10:40:59 +0200

    Build 4 versions, sequential, threaded, blacs, hybrid

--- Math/Lapack/SVD/build.sh
+++ Math/Lapack/SVD/build.sh
@@ -1,13 +1,23 @@
 #!/bin/bash -l
 
-module purge
+source switch_to_2014a
 module load intel/2014a
 module load HDF5/1.8.12-intel-2014a-serial
 
-FFLAGS="-O2 -g -openmp -I${MKLROOT}/include"
-MATH_LIBS="-L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -lpthread -lm"
+FFLAGS="-O2 -g -I${MKLROOT}/include"
+OMP="-openmp"
+
+MATH_LIBS=" -L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lpthread -lm"
+
+THREADED_MATH_LIBS=" -L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -lpthread -lm"
 
 DISTR_MATH_LIBS=" -L${MKLROOT}/lib/intel64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lmkl_blacs_intelmpi_lp64 -lpthread -lm"
 
-make F90=ifort MPIF90=mpiifort FFLAGS="${FFLAGS}" HDF5_DIR="${EBROOTHDF5}" \
-     MATH_LIBS="${MATH_LIBS}" DISTR_MATH_LIBS="${DISTR_MATH_LIBS}" all
+HYBRID_MATH_LIBS="  -L${MKLROOT}/lib/intel64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -lmkl_blacs_intelmpi_lp64 -lpthread -lm"
+make F90=ifort MPIF90=mpiifort FFLAGS="${FFLAGS}" OMP="${OMP}" \
+     HDF5_DIR="${EBROOTHDF5}" \
+     MATH_LIBS="${MATH_LIBS}" \
+     THREADED_MATH_LIBS="${THREADED_MATH_LIBS}" \
+     DISTR_MATH_LIBS="${DISTR_MATH_LIBS}" \
+     HYBRID_MATH_LIBS="${HYBRID_MATH_LIBS}" \
+     all
--- Math/Lapack/SVD/build_2015a.sh
+++ Math/Lapack/SVD/build_2015a.sh
@@ -1,16 +1,23 @@
 #!/bin/bash -l
 
-module purge
 source switch_to_2015a
 module load intel/2015a
 module load HDF5/1.8.14-intel-2015a-serial
 
-FFLAGS="-O2 -g -openmp -I${MKLROOT}/include"
+FFLAGS="-O2 -g -I${MKLROOT}/include"
+OMP="-openmp"
+
 MATH_LIBS=" -L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lpthread -lm"
+
+THREADED_MATH_LIBS=" -L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -lpthread -lm"
 
 DISTR_MATH_LIBS=" -L${MKLROOT}/lib/intel64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_sequential -lmkl_blacs_intelmpi_lp64 -lpthread -lm"
 
 HYBRID_MATH_LIBS="  -L${MKLROOT}/lib/intel64 -lmkl_scalapack_lp64 -lmkl_intel_lp64 -lmkl_core -lmkl_intel_thread -lmkl_blacs_intelmpi_lp64 -lpthread -lm"
-make F90=ifort MPIF90=mpiifort FFLAGS="${FFLAGS}" HDF5_DIR="${EBROOTHDF5}" \
-     MATH_LIBS="${MATH_LIBS}" DISTR_MATH_LIBS="${DISTR_MATH_LIBS}" \
-     HYBRID_MATH_LIBS="${HYBRID_MATH_LIBS}" all
+make F90=ifort MPIF90=mpiifort FFLAGS="${FFLAGS}" OMP="${OMP}" \
+     HDF5_DIR="${EBROOTHDF5}" \
+     MATH_LIBS="${MATH_LIBS}" \
+     THREADED_MATH_LIBS="${THREADED_MATH_LIBS}" \
+     DISTR_MATH_LIBS="${DISTR_MATH_LIBS}" \
+     HYBRID_MATH_LIBS="${HYBRID_MATH_LIBS}" \
+     all
