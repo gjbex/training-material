066aa23455423bcc14e6521a3c00d7eec900abaa
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-10-29 19:00:51 +0100

    Removed some unnecessary brackets

--- Python/Interfacing_C_C++_Fortran/comp_pi_f2py.py
+++ Python/Interfacing_C_C++_Fortran/comp_pi_f2py.py
@@ -5,7 +5,7 @@ from comp_pi import compute_pi
 
 
 def main():
-    n = (int(sys.argv[1]) if len(sys.argv) > 1 else 1000)
+    n = int(sys.argv[1]) if len(sys.argv) > 1 else 1000
     print(compute_pi(n))
     return 0
 
