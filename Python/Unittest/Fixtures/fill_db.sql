b33f2f67117a0cedc10b7dc7a6463a3ba1c627ee
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-12-15 08:25:30 +0100

    Added code to insert initial data into database

---
+++ Python/Unittest/Fixtures/fill_db.sql
@@ -0,0 +1,13 @@
+INSERT INTO projects
+    (project_name, start_date, end_date)
+VALUES
+    ('project 1', '2014-10-14', '2015-02-04'),
+    ('project 2', '2014-10-24', '2015-05-09'),
+    ('project 3', '2014-11-04', '2015-03-24');
+
+INSERT INTO researchers
+    (first_name, last_name)
+VALUES
+    ('Alice', 'Perry'),
+    ('Bob', 'Dreary'),
+    ('Carol', 'Flynn');
