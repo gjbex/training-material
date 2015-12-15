8f48a36043c344d6891686377aaa64b04d68667b
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-12-15 18:45:19 +0100

    Added delete triggers to keep staff assignments in sync with researchers and projects

--- Python/Unittest/Fixtures/create_db.sql
+++ Python/Unittest/Fixtures/create_db.sql
@@ -1,5 +1,5 @@
+-- Create projects table
 DROP TABLE IF EXISTS projects;
-
 CREATE TABLE projects (
     project_id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
     project_name TEXT NOT NULL,
@@ -9,16 +9,17 @@ CREATE TABLE projects (
                                      start_date < end_date)
 );
 
+-- Create researchers table
 DROP TABLE IF EXISTS researchers;
-
 CREATE TABLE researchers (
     researcher_id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL
 );
 
+-- Create staff assignments table, encoding the many-to-many relation
+-- between researchers and projects
 DROP TABLE IF EXISTS staff_assignments;
-
 CREATE TABLE staff_assignments (
     project_id INTEGER NOT NULL,
     researcher_id INTEGER NOT NULL,
@@ -27,12 +28,35 @@ CREATE TABLE staff_assignments (
     FOREIGN KEY (researcher_id) REFERENCES researchers(researcher_id)
 );
 
-DROP VIEW IF EXISTS project_staffing;
+-- Ensure that when a researcher is deleted, all staff assignments
+-- he is involved in are also deleted
+DROP TRIGGER IF EXISTS delete_researcher_assignemnt;
+CREATE TRIGGER delete_researcher_assignemnt
+    DELETE ON researchers
+    FOR EACH ROW
+    BEGIN
+        DELETE FROM staff_assignments
+            WHERE OLD.researcher_id = researcher_id;
+    END;
 
+-- Ensure that when a project is deleted, all staff assignments
+-- he is involved in are also deleted
+DROP TRIGGER IF EXISTS delete_project_assignemnt;
+CREATE TRIGGER delete_project_assignemnt
+    DELETE ON projects
+    FOR EACH ROW
+    BEGIN
+        DELETE FROM staff_assignments
+            WHERE OLD.project_id = project_id;
+    END;
+
+-- Create a view to get a more convenient overview of staff assignments
+DROP VIEW IF EXISTS project_staffing;
 CREATE VIEW project_staffing
-    (project_name, first_name, last_name)
-AS SELECT p.project_name, r.first_name, r.last_name
-    FROM projects AS p, researchers AS r, staff_assignments AS s
-    WHERE p.project_id = s.project_id AND
-          s.researcher_id = r.researcher_id AND
-          s.researcher_id = r.researcher_id;
+    AS SELECT p.project_name AS project_name,
+              r.first_name AS 'first_name',
+              r.last_name AS 'last_name'
+            FROM projects AS p, researchers AS r, staff_assignments AS s
+            WHERE p.project_id = s.project_id AND
+                  s.researcher_id = r.researcher_id AND
+                  s.researcher_id = r.researcher_id;
