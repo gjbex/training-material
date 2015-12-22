686f1e07c395a61b6c36692f2eaf6164a47e7097
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-12-23 00:17:21 +0100

    Added add comman to shell

--- Python/DbAccess/ExperimentDB/shell.py
+++ Python/DbAccess/ExperimentDB/shell.py
@@ -2,9 +2,14 @@
 
 from argparse import ArgumentParser
 import cmd
+import shlex
 import sys
-from experiments import Experiment, Researcher, Sample
+from experiments import Experiment, Researcher, Sample, staff_assignments
 from orm_utils import create_session
+
+
+class SyntaxException(Exception):
+    pass
 
 
 class ExperimentShell(cmd.Cmd):
@@ -20,20 +25,109 @@ class ExperimentShell(cmd.Cmd):
         '''quit the shell'''
         return True
 
-    def do_show(self, arg_str):
-        '''show either projects, researchers or samples'''
-        if arg_str == 'projects':
-            for experiment in self._db_session.query(Experiment).all():
-                print(experiment)
-        elif arg_str == 'researchers':
-            for researcher in self._db_session.query(Researcher).all():
-                print(researcher)
-        elif arg_str == 'samples':
-            for sample in self._db_session.query(Sample).all():
-                print(sample)
+    @staticmethod
+    def parse_show_arg(arg_str):
+        classes = {
+            'experiments': Experiment,
+            'researchers': Researcher,
+            'samples': Sample
+        }
+        args = shlex.split(arg_str)
+        if len(args) != 1 and len(args) != 3:
+            msg = 'Expecting show <table> [ for <id> ]'
+            raise SyntaxException(msg)
+        cls_str = args[0]
+        if cls_str not in classes:
+            msg = "Illegal keyword '{0}' for show".format(cls_str)
+            raise SyntaxException(msg)
+        cls = classes[cls_str]
+        if len(args) > 1 and args[1] != 'for':
+            msg = "Syntax error, expected for, not '{0}'".format(cls_str)
+            raise SyntaxException(msg)
+        if len(args) == 3:
+            try:
+                item_id = int(args[2])
+            except:
+                msg = "Syntax error, expected ID, not '{0}'".format(args[2])
+                raise SyntaxException(msg)
         else:
-            print("illegal keyword '{0}' for show".format(arg_str),
-                  file=sys.stderr)
+            item_id = None
+        return cls, item_id
+
+    def do_show(self, arg_str):
+        '''Use either:
+           show experiments [ for <researcher_id> ]
+           show researchers [ for <experiment_id> ]
+           show samples [ for <experiment_id> ]'''
+        try:
+            cls, item_id = ExperimentShell.parse_show_arg(arg_str)
+        except SyntaxException as e:
+            print('*** {0}'.format(str(e)), file=sys.stderr)
+            return
+        if not item_id:
+            items = self._db_session.query(cls).all()
+        else:
+            if cls == Experiment:
+                items = self._db_session.\
+                             query(Experiment).\
+                             join(staff_assignments).\
+                             filter_by(researcher_id=item_id).all()
+            elif cls == Researcher:
+                items = self._db_session.\
+                             query(Researcher).\
+                             join(staff_assignments).\
+                             filter_by(experiment_id=item_id).all()
+            elif cls == Sample:
+                items = self._db_session.\
+                             query(Sample).\
+                             filter_by(experiment_id=item_id).all()
+        for item in items:
+            print(item)
+
+    @staticmethod
+    def parse_add_arg(arg_str):
+        types = {'experiment', 'researcher', 'sample'}
+        args = shlex.split(arg_str)
+        if len(args) != 3:
+            msg = 'Expecting add <type> <attr1> <attr2>'
+            raise SyntaxException(msg)
+        if args[0] not in types:
+            msg = "Illegal keyword '{0}' for add".format(args[0])
+            raise SyntaxException(msg)
+        if args[0] == 'sample':
+            try:
+                args[2] = int(args[2])
+            except:
+                msg = "experiment ID must be int, not '{0]}".format(args[2])
+                raise SyntaxException(msg)
+        return args[0], args[1], args[2]
+
+    def do_add(self, arg_str):
+        '''Use either:
+           add experiment <description> <start_date>
+           add researcher <last_name> <first_name>
+           add sample <description> <experiment_id>'''
+        try:
+            type_str, attr1, attr2 = ExperimentShell.parse_add_arg(arg_str)
+        except SyntaxException as e:
+            print('*** {0}'.format(str(e)), file=sys.stderr)
+            return
+        if type_str == 'experiment':
+            item = Experiment(description=attr1, start_date=attr2)
+        elif type_str == 'researcher':
+            item = Researcher(last_name=attr1, first_name=attr2)
+        elif type_str == 'sample':
+            item = Sample(description=attr1, experiment_id=attr2)
+        self._db_session.add(item)
+        self._db_session.commit()
+
+    @staticmethod
+    def parse_assign_arg(arg_str):
+        pass
+
+    def do_assign(self, arg_str):
+        '''Use: assign <researcher_id> to <experiment_id>'''
+        pass
 
 
 if __name__ == '__main__':
