79ef74f3fee81f8ad7f7b1b70b08399bb9e7b9bc
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-12-31 19:35:53 +0100

    Added tests for tree accessors

---
+++ C/Tree_2k/src/test/accessor_tests.h
@@ -0,0 +1,7 @@
+#ifndef _ACCESSOR_TESTS_HDR_
+#define _ACCESSOR_TESTS_HDR_
+
+void test_basic_accessors(void);
+void test_data_accessors(void);
+
+#endif
--- C/Tree_2k/src/test/test_driver.c
+++ C/Tree_2k/src/test/test_driver.c
@@ -1,32 +1,54 @@
 #include <err.h>
 #include <stdlib.h>
 #include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
 
-#include "init_tests.h"
-#include "insert_tests.h"
+void add_init_suite(void);
+void add_insert_suite(void);
+void add_accessor_suite(void);
 
 int main(int argc, char *argv[]) {
     int status = CU_initialize_registry();
     CU_pSuite init_suite, insert_suite;
     if (status != CUE_SUCCESS)
         errx(status, "CUnit registry initialization failed");
-    init_suite = CU_add_suite("init", NULL, NULL);
-    CU_add_test(init_suite, "basic tree allocation",
-                &test_tree_alloc);
-    CU_add_test(init_suite, "tree allocation rank failure",
-                &test_tree_alloc_rank_0);
-    CU_add_test(init_suite, "tree allocation invalid extent failure",
-                &test_tree_alloc_invalid_extent);
-    insert_suite = CU_add_suite("insert", NULL, NULL);
-    CU_add_test(insert_suite, "single insert",
-                &test_single_insert);
-    CU_add_test(insert_suite, "multiple inserts, no split",
-                &test_multiple_insert_no_split);
-    CU_add_test(insert_suite, "multiple inserts, with split",
-                &test_multiple_insert_with_split);
-    CU_add_test(insert_suite, "failed insert, not in extent",
-                &test_failed_insert);
+    add_init_suite();
+    add_insert_suite();
+    add_accessor_suite();
     CU_basic_run_tests();
     CU_cleanup_registry();
     return EXIT_SUCCESS;
+}
+
+#include "init_tests.h"
+void add_init_suite(void) {
+    CU_pSuite suite = CU_add_suite("init", NULL, NULL);
+    CU_add_test(suite, "basic tree allocation",
+                &test_tree_alloc);
+    CU_add_test(suite, "tree allocation rank failure",
+                &test_tree_alloc_rank_0);
+    CU_add_test(suite, "tree allocation invalid extent failure",
+                &test_tree_alloc_invalid_extent);
+}
+
+#include "insert_tests.h"
+void add_insert_suite(void) {
+    CU_pSuite suite = CU_add_suite("insert", NULL, NULL);
+    CU_add_test(suite, "single insert",
+                &test_single_insert);
+    CU_add_test(suite, "multiple inserts, no split",
+                &test_multiple_insert_no_split);
+    CU_add_test(suite, "multiple inserts, with split",
+                &test_multiple_insert_with_split);
+    CU_add_test(suite, "failed insert, not in extent",
+                &test_failed_insert);
+}
+
+#include "accessor_tests.h"
+void add_accessor_suite(void) {
+    CU_pSuite suite = CU_add_suite("accessors", NULL, NULL);
+    CU_add_test(suite, "basic accessors",
+                &test_basic_accessors);
+    CU_add_test(suite, "data accessors",
+                &test_data_accessors);
 }
