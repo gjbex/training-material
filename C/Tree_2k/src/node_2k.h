0d963688974139970ba1d91e427c8bf717992910
Geert Jan BEX <geertjan.bex@uhasselt.be>
2016-01-08 05:04:36 +0100

    Added post-order tree walk function

--- C/Tree_2k/src/node_2k.h
+++ C/Tree_2k/src/node_2k.h
@@ -32,5 +32,8 @@ bool node_2k_can_have_points(node_2k_t *node, const double *coords,
                              double radius);
 tree_2k_err_t node_2k_query(node_2k_t *node, node_2k_list_t *query_result,
                             const double *coords, double radius);
+tree_2k_err_t node_2k_walk(node_2k_t *node,
+                           int (*f) (node_2k_t *, void *),
+                           void *x);
 
 #endif
