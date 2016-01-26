1b696c5d0ca405ee17c03ef7db731df8aaf25f6d
Geert Jan BEX <geertjan.bex@uhasselt.be>
2016-01-26 06:55:58 +0100

    Improved all to all example

--- Python/Mpi4py/all_to_all.py
+++ Python/Mpi4py/all_to_all.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
-    send_buffer = [(rank + 2)**i for i in range(size)]
+    send_buffer = [rank*size + i for i in range(size)]
     data = comm.alltoall(send_buffer)
     print('rank {0}: {1}'.format(rank, ', '.join((str(x) for x in data))))
