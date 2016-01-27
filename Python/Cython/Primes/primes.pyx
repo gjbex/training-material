1659a86acec9bf4fb1c3a92be90ce47a1d9e759d
Geert Jan BEX <geertjan.bex@uhasselt.be>
2016-01-27 15:49:15 +0100

    Removed useless code from cython file

--- Python/Cython/Primes/primes.pyx
+++ Python/Cython/Primes/primes.pyx
@@ -16,12 +16,3 @@ def primes(int kmax):
             result.append(n)
         n = n + 1
     return result
-
-if __name__ == '__main__':
-    from argparse import ArgumentParser
-    arg_parser = ArgumentParser(description='compute primes')
-    arg_parser.add_argument('n', type=int, default=10,
-                            help='number of primes')
-    options = arg_parser.parse_args()
-    results = primes(options.n)
-    print(', '.join(results))
