1d4862c1022cadfaf0b0bd03763fe3af3a80c3a6
Geert Jan BEX <geertjan.bex@uhasselt.be>
2016-01-28 13:58:49 +0100

    Fixed style issues

--- Python/Ising/util.py
+++ Python/Ising/util.py
@@ -2,8 +2,8 @@
 
 options_fmt = '{prefix:s}{key:s} = {value:s}\n'
 
+
 def print_options(file, options, prefix='# '):
     for key, value in options.__dict__.items():
         file.write(options_fmt.format(prefix=prefix, key=key,
                                       value=str(value)))
-
