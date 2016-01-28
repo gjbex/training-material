306fe646779be3f1348926cc44a9319242c9fa92
Geert Jan BEX <geertjan.bex@uhasselt.be>
2016-01-28 13:45:04 +0100

    Fixed style issues

--- Python/Ising/ising_demo.py
+++ Python/Ising/ising_demo.py
@@ -2,15 +2,13 @@
 
 
 if __name__ == '__main__':
-
     from argparse import ArgumentParser
-    import random, sys
-
+    import random
+    import sys
     import numpy as np
-
     from ising_cxx import IsingSystem
     from runner import (SingleRunner, MovieRunner, ActivityHeatmapRunner,
-                        EquilibriumRunner, UnknownQuantityError)
+                        EquilibriumRunner)
 
     arg_parser = ArgumentParser(description='2D ising system simulaation')
     arg_parser.add_argument('--N', type=int, default=10,
@@ -66,4 +64,3 @@ if __name__ == '__main__':
     elif options.mode == 'equilibrium':
         for quantity in runner.quantities():
             print('{0:s} = {1:.4e}'.format(quantity, runner.get(quantity)))
-
