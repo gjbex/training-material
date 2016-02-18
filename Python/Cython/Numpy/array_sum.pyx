78c8c66c91d288f40b813c28cff450ef4213aaef
Geert Jan BEX <geertjan.bex@uhasselt.be>
2016-02-18 17:46:37 +0100

    Added return type to C function

--- Python/Cython/Numpy/array_sum.pyx
+++ Python/Cython/Numpy/array_sum.pyx
@@ -5,7 +5,7 @@ def array_sum(a):
     return _array_sum(memoryview(a))
 
 
-cdef _array_sum(double[:,::1] mem_view):
+cdef double _array_sum(double[:,::1] mem_view):
     cdef double total = 0.0
     cdef m = mem_view.shape[0]
     cdef n = mem_view.shape[1]
