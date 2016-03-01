c7b8537e700807c641ae168cb0b8ce449cf778c9
Geert Jan BEX <geertjan.bex@uhasselt.be>
2016-03-01 15:42:37 +0100

    Extended C99 VLA example a bit

--- C/C99/vla.c
+++ C/C99/vla.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void sqr(double a[], int n);
+
 int main(int argc, char *argv[]) {
     int n = 10;
     if (argc > 1)
@@ -11,5 +13,13 @@ int main(int argc, char *argv[]) {
         data[i] = (double) i;
     for (int i = 0; i < n; i++)
         printf("sqrt(%lf) = %lf\n", data[i], sqrt(data[i]));
+    sqr(data, n);
+    for (int i = 0; i < n; i++)
+        printf("%d: %lf\n", i, data[i]);
     return EXIT_SUCCESS;
+}
+
+void sqr(double a[], int n) {
+    for (int i = 0; i < n; i++)
+        a[i] = a[i]*a[i];
 }
