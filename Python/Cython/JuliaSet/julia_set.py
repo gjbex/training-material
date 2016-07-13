e403eedf55c85cbe51fa36d2ebec7dc4ec8f6128
Geert Jan Bex <geertjan.bex@uhasselt.be>
2016-07-13 10:23:18 +0200

    Changed approach

--- Python/Cython/JuliaSet/julia_set.py
+++ Python/Cython/JuliaSet/julia_set.py
@@ -4,34 +4,36 @@ from argparse import ArgumentParser
 import numpy as np
 
 
-def julia(z):
-    '''Complex function to iterate.'''
-    return z**2 - 0.622772 + 0.42193j
-
-
-def update_domain(domain, iterations, max_norm):
-    domain[np.absolute(domain) <= max_norm] = julia(domain)
-    iterations[np.absolute(domain) <= max_norm] += 1
-
-
-def julia_set(re, im, n, max_norm, max_iters):
-    '''Compute the Julia set on a complex domain.
+def init_julia(re, im, n):
+    '''Initialize the complex domain.
 
     Positional arguments:
-    re -- limits of the real part of the domain as 2-tuple
-    im -- limits of the complex part of the domain as 2-tuple
-    n -- number of points on the real and the imaginary axis as tuple
-    max_norm -- maximum complex norm to iterate to.
-    max_iters -- maximum number of iterations
+    re -- minimum and maximum real value as 2-tuple
+    im -- minimum and maximum imaginary value as 2-tuple
+    n -- number of real and imaginary points as 2-tuple
     '''
     re_vals, im_vals = np.meshgrid(
             np.linspace(re[0], re[1], n[0]),
             np.linspace(im[0], im[1], n[1])
             )
     domain = re_vals + im_vals*1j
-    iterations = np.zeros(n, dtype=int)
-    for iter_nr in max_iters:
-        update_domain(domain, iterations, max_norm)
+    return domain.flatten()
+
+
+def julia_set(domain, iteratons, max_norm, max_iters):
+    '''Compute the Julia set on a complex domain.
+
+    Positional arguments:
+    domain -- complex domain to compute a 1-D array
+    iterations -- number of iterations as a 1-D array
+    max_norm -- maximum complex norm to iterate to.
+    max_iters -- maximum number of iterations
+    '''
+    for i in range(len(domain)):
+        z = domain[i]
+        while iterations[i] <= max_iters and np.absolute(z) <= max_norm:
+            z = z**2 - 0.622772 + 0.42193j
+            iterations[i] += 1
     return iterations
 
 if __name__ == '__main__':
@@ -55,13 +57,14 @@ if __name__ == '__main__':
     arg_parser.add_argument('--show', action='store_true',
                             help='show plot')
     options = arg_parser.parse_args()
-    iterations = julia_set(
+    domain = init_julia(
             (options.re_min, options.re_max),
             (options.im_min, options.im_max),
-            (options.n_re, options.n_im),
-            options.max_norm, options.max_iters
+            (options.n_re, options.n_im)
             )
+    iterations = np.zeros(options.n_re*options.n_im, dtype=int)
+    julia_set(domain, iterations, options.max_norm, options.max_iters)
     if options.show:
-        import matplotlib.pyplt as plt
-        plt.imshow(iterations)
+        import matplotlib.pyplot as plt
+        plt.imshow(iterations.reshape(options.n_re, options.n_im))
         plt.show()
