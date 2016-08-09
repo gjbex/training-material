095fb6ec54c66c73a42ddcfa39b9a9cd8790a3d1
Geert Jan Bex <geertjan.bex@uhasselt.be>
2016-08-09 08:18:21 +0200

    Added some features; renamed variables to reflect domain

--- Python/Biopython/align_seqs.py
+++ Python/Biopython/align_seqs.py
@@ -12,22 +12,31 @@ if __name__ == '__main__':
     arg_parser.add_argument('file', help='sequence file')
     arg_parser.add_argument('--format', default='fasta',
                             help='input format')
-    arg_parser.add_argument('--out', required=False, help='output file')
+    arg_parser.add_argument('--alignment', help='input file is an '
+                                                'alignment')
+    arg_parser.add_argument('--show', action='store_true',
+                            help='show MUSCLE output')
     options = arg_parser.parse_args()
     seqs = {}
     for seq_record in SeqIO.parse(options.file, options.format):
         seqs[seq_record.id] = seq_record.seq
-    if options.out:
-        muscle_cl = MuscleCommandline(input=options.file, out=options.out,
-                                      clw=True)
-        muscle_cl()
+    if options.alignment:
+        with open(options.alignment, 'r') as alignment_file:
+            stdout = alignment_file.read()
     else:
         muscle_cl = MuscleCommandline(input=options.file)
         stdout, stderr = muscle_cl()
-        alignments = AlignIO.read(StringIO(stdout), 'fasta')
-        print('number of alignments: {0:d}'.format(len(alignments)))
-        msg = 'id: {0}\n\tlength: {1:d}\n\tgaps: {2:d}'
-        for alignment in alignments:
-            print(msg.format(alignment.id,
-                             len(str(seqs[alignment.id])),
-                             str(alignment.seq).count('-')))
+    if options.show:
+        print(stdout)
+    else:
+        alignment = AlignIO.read(StringIO(stdout), 'fasta')
+        print('number of sequences: {0:d}'.format(len(alignment)))
+        print('length: {0:d}'.format(alignment.get_alignment_length()))
+        msg = 'id: {0}\n\tlength: {1:d}\n\tgaps: {2:d}\n\ttotal: {3:d}'
+        for seq_rec in alignment:
+            seq_length = len(seqs[seq_rec.id])
+            nr_gaps = str(seq_rec.seq).count('-')
+            print(msg.format(seq_rec.id, seq_length, nr_gaps,
+                             seq_length + nr_gaps))
+        for seq_rec in alignment:
+            print(seq_rec.seq[:76])
