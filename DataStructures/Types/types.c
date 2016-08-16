1bbfffe2b302d84489c322299bc648166f9d0fad
Geert Jan Bex <geertjan.bex@uhasselt.be>
2016-08-16 08:55:31 +0200

    Added first version of program to show type information

---
+++ DataStructures/Types/types.c
@@ -0,0 +1,10 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("int: %d .. %d\n", INT_MIN, INT_MAX);
+    printf("long: %ld .. %ld\n", LONG_MIN, LONG_MAX);
+    return EXIT_SUCCESS;
+}
