b9566900a826f9b25bca6512a7578d8e4f264b51
Geert Jan Bex <geertjan.bex@uhasselt.be>
2016-08-20 01:27:45 +0200

    Using 64-bit mt19937

--- CPlusPlus/Random/random_double.cpp
+++ CPlusPlus/Random/random_double.cpp
@@ -6,11 +6,11 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    mt19937::result_type seed = 1234;
+    mt19937_64::result_type seed = 1234;
     if (argc > 1)
         seed = atoi(argv[1]);
     auto distribution = bind(uniform_real_distribution<double>(0.0, 1.0),
-                             mt19937(seed));
+                             mt19937_64(seed));
     int n = 10;
     if (argc > 2)
         n = atoi(argv[2]);
