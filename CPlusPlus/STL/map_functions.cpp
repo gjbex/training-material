5367305525ff8e7178de289382a0f72888c65dec
gjbex <geertjan.bex@uhasselt.be>
2016-08-26 16:52:02 +0200

    Added call with dynamically allocated map; cleaned up types

--- CPlusPlus/STL/map_functions.cpp
+++ CPlusPlus/STL/map_functions.cpp
@@ -4,22 +4,27 @@
 #include <unordered_map>
 
 typedef std::map<std::string, int> Map;
+typedef std::unordered_map<std::string, int> Unordered_map;
 typedef std::list<std::string> List;
 
 template <typename Map_type>
 void fill_map(Map_type& map, const List& list);
 
 template <typename Map_type>
-void print_map(const Map_type& map, std::string label);
+void print_map(const Map_type& map, const std::string& label);
 
 int main(int argc, char *argv[]) {
-    std::list<std::string> keys = {"a", "b", "c"};
+    List keys = {"a", "b", "c"};
     Map si_map;
-    std::unordered_map<std::string, int> usi_map;
+    Unordered_map usi_map;
+    Unordered_map *usi_map_p = new Unordered_map();
     fill_map(si_map, keys);
     fill_map(usi_map, keys);
+    fill_map(*usi_map_p, keys);
     print_map(si_map, "map");
     print_map(usi_map, "unordered_map");
+    print_map(*usi_map_p, "map pointer");
+    delete usi_map_p;
     return 0;
 }
 
@@ -31,7 +36,7 @@ void fill_map(Map_type& map, const List& list) {
 }
 
 template <typename Map_type>
-void print_map(const Map_type& map, std::string label) {
+void print_map(const Map_type& map, const std::string& label) {
     std::cout << label << ":" << std::endl;
     for (auto iter = map.begin(); iter != map.end(); iter++)
         std::cout << iter->first << " -> " << iter->second
