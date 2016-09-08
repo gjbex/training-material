8c79faf97f0a908a93e72161069b7d7d7b6ba99b
Geert Jan Bex <geertjan.bex@uhasselt.be>
2016-09-08 10:53:34 +0200

    Added to Boost graph example

--- CPlusPlus/Boost/Graph/family_tree.cpp
+++ CPlusPlus/Boost/Graph/family_tree.cpp
@@ -9,11 +9,17 @@ typedef std::pair<int, int> Edge;
 typedef boost::adjacency_list<boost::vecS, boost::vecS,
                               boost::bidirectionalS> Graph;
 typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
+typedef boost::property_map<Graph, boost::vertex_index_t> Index_map;
+typedef boost::graph_traits<Graph>::vertex_iterator Vertex_iter;
 
 // type definitions for auxilery data structures
 typedef std::list<std::string> Name_list;
 typedef std::unordered_map<int, std::string> Id_map;
 typedef std::unordered_map<std::string, int> Name_map;
+typedef std::pair<Id_map, Name_map> Maps;
+
+Maps& create_maps(const Name_list& names);
+void print_vertices(const Graph& grapph, const Name_map& name_map);
 
 // names of the family members
 const Name_list names = {
@@ -23,15 +29,9 @@ const Name_list names = {
 int main(int argc, char *argv[]) {
     std::size_t nr_vertices = names.size();
     // for convenience, map between numerical IDs/names, and vice versa
-    Id_map id_map;
-    Name_map name_map;
-    int id = 0;
-    for (auto it = names.begin(); it != names.end(); it++) {
-        std::string name = *it;
-        id_map[id] = name;
-        name_map[name] = id;
-        id++;
-    }
+    Maps maps = create_maps(names);
+    Id_map id_map = maps.first;
+    Name_map name_map = maps.second;
     // define the graph's edges as an array, and determine the number of
     // edges
     Edge is_child_of[] = {
@@ -49,5 +49,31 @@ int main(int argc, char *argv[]) {
     boost::topological_sort(g, std::front_inserter(family_lines));
     for (auto it = family_lines.begin(); it != family_lines.end(); it++)
         std::cout << id_map[*it] << std::endl;
+    // print all vertices
+    Index_map index_map = boost::get(Vertex_index, g); 
+    std::pair<Vertex_iter, Vertex_iter> iters;
+    for (iters = boost::vertices(g); iters.first != iters.second;
+            iters.first++) {
+        Vertex v = *iters.first;
+        std::cout << index_map[v] << std::endl;
+    }
     return 0;
+}
+
+Maps& create_maps(const Name_list& names) {
+    Id_map id_map;
+    Name_map name_map;
+    int id = 0;
+    for (auto it = names.begin(); it != names.end(); it++) {
+        std::string name = *it;
+        id_map[id] = name;
+        name_map[name] = id;
+        id++;
+    }
+    Maps *maps = new Maps(id_map, name_map);
+    return *maps;
+}
+
+void print_vertices(const Graph& grapph, const Name_map& name_map) {
+    
 }
