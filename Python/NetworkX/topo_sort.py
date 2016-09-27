dfd1a213679b4703e29a425610e8498f19a83fa1
Geert Jan Bex <geertjan.bex@uhasselt.be>
2016-09-27 06:50:12 +0200

    Added example on topological sort

--- Python/NetworkX/topo_sort.py
+++ Python/NetworkX/topo_sort.py
@@ -11,6 +11,7 @@ class Reference(object):
         self._title = title
         self._journal = journal
         self._year = year
+        self._key = None
 
     @property
     def author(self):
@@ -28,6 +29,22 @@ class Reference(object):
     def year(self):
         return self._year
 
+    @property
+    def key(self):
+        return self._key
+
+    @key.setter
+    def key(self, ref_key):
+        self._key = ref_key
+
+    def format(self):
+        fmt_str = '[{key}] {author} "{title}", {journal}, {year}'
+        return fmt_str.format(key=self.key, author=self.author,
+                              title=self.title, journal=self.journal,
+                              year=self.year)
+    def __str__(self):
+        return self.key
+
 
 def create_refs():
     refs = {
@@ -35,56 +52,65 @@ def create_refs():
                     author='Alice',
                     title='Encryption with blowfish',
                     journal='Security research',
-                    year='2001',),
+                    year=2001,),
         'B02a': Reference(
                     author='Bob',
                     title='Applying blowfish for fun and profit',
                     journal='Security research',
-                    year='2002',),
+                    year=2002,),
         'B02b': Reference(
                     author='Bob',
                     title='Applying blowfish for email encryption',
                     journal='Security research',
-                    year='2002',),
+                    year=2002,),
         'C03': Reference(
                     author='Carol',
                     title='Pufferfish: a new encryption algorithm',
                     journal='Notices of the Luxemburg Academy of Sciences',
-                    year='2003',),
+                    year=2003,),
         'E02': Reference(
                     author='Eve',
                     title='Hacking blowfish',
                     journal='Hacker magazine',
-                    year='2002',),
+                    year=2002,),
         'T04': Reference(
                     author='Trent',
                     title='Review of modern encryption systems',
                     journal='ACM Research on Security',
-                    year='2004',),
+                    year=2004,),
         'A03': Reference(
                     author='Alice',
                     title='Proving blowfish to be mathemattically secure',
                     journal='Security research',
-                    year='2003',),
+                    year=2003,),
     }
+    for key in refs:
+        refs[key].key = key
     return refs
 
 
 def create_graph():
     refs = create_refs()
     citations = nx.DiGraph()
-    citations.add_edge(refs['A00'], refs['B02a'])
-    citations.add_edge(refs['A00'], refs['B02b'])
-    citations.add_edge(refs['A00'], refs['E02'])
-    citations.add_edge(refs['A00'], refs['A03'])
-    citations.add_edge(refs['A00'], refs['T04'])
+    citations.add_edge(refs['A01'], refs['B02a'])
+    citations.add_edge(refs['A01'], refs['B02b'])
+    citations.add_edge(refs['A01'], refs['E02'])
+    citations.add_edge(refs['A01'], refs['A03'])
+    citations.add_edge(refs['A01'], refs['T04'])
     citations.add_edge(refs['B02a'], refs['B02b'])
     citations.add_edge(refs['B02a'], refs['T04'])
     citations.add_edge(refs['E02'], refs['T04'])
     citations.add_node(refs['C03'])
     return citations
 
+
+def show_sorted(G):
+    nodes = nx.algorithms.topological_sort(G)
+    print('\n'.join([n.format() for n in nodes]))
+
 if __name__ == '__main__':
     arg_parser = ArgumentParser(description='illustration of topological '
                                             'sort')
     options = arg_parser.parse_args()
+    G = create_graph()
+    show_sorted(G)
