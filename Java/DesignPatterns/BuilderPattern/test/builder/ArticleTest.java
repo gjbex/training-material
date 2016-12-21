67dd9bf8fe6cae819e880969ecc3c5b37f5b13c2
Geert Jan Bex <geertjan.bex@uhasselt.be>
2016-12-21 18:00:59 +0100

    Added tests

--- Java/DesignPatterns/BuilderPattern/test/builder/ArticleTest.java
+++ Java/DesignPatterns/BuilderPattern/test/builder/ArticleTest.java
@@ -8,6 +8,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.sun.org.apache.xalan.internal.extensions.ExpressionContext;
+
 import builder.Article.ArticleBuilder;
 
 /**
@@ -41,6 +43,57 @@ public class ArticleTest {
 			builder.setYear(year);
 			Article article = builder.build();
 			assertEquals(title, article.getTitle());
+		} catch (InvalidArgumentException e) {
+			fail("no " + e.getClass().getName() + " exception should be thrown");
+		} catch (UninitializedArticleException e) {
+			fail("no " + e.getClass().getName() + " exception should be thrown");
+		}
+	}
+
+	@Test(expected = InvalidArgumentException.class)
+	public void testInvalidYear() throws Exception {
+		builder.setYear(-1);
+	}
+	
+	@Test(expected = UninitializedArticleException.class)
+	public void testUninitialized() throws Exception {
+		builder.setYear(2016).build();
+	}
+
+	@Test
+	public void testNoOptionals() {
+		try {
+			builder.setAuthors(authors);
+			builder.setTitle(title);
+			builder.setJournal(journal);
+			builder.setIssue(issue);
+			builder.setPages(pages);
+			builder.setYear(year);
+			Article article = builder.build();
+			assertEquals(title, article.getTitle());
+			assertFalse(article.hasDOI());
+			assertFalse(article.hasURL());
+		} catch (InvalidArgumentException e) {
+			fail("no " + e.getClass().getName() + " exception should be thrown");
+		} catch (UninitializedArticleException e) {
+			fail("no " + e.getClass().getName() + " exception should be thrown");
+		}
+	}
+
+	@Test
+	public void testOptionalDOI() {
+		try {
+			builder.setAuthors(authors);
+			builder.setTitle(title);
+			builder.setJournal(journal);
+			builder.setIssue(issue);
+			builder.setPages(pages);
+			builder.setYear(year);
+			builder.setDOI("10.1494549a");
+			Article article = builder.build();
+			assertEquals(title, article.getTitle());
+			assertTrue(article.hasDOI());
+			assertFalse(article.hasURL());
 		} catch (InvalidArgumentException e) {
 			fail("no " + e.getClass().getName() + " exception should be thrown");
 		} catch (UninitializedArticleException e) {
