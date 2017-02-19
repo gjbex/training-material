658ebd304ef626b4b0930dbe27543cb43d0ef27f
Geert Jan Bex <geertjan.bex@uhasselt.be>
2017-02-20 00:45:32 +0100

    Removed charge atribute

--- CPlusPlus/UserDefinedTypes/class_particles.cpp
+++ CPlusPlus/UserDefinedTypes/class_particles.cpp
@@ -9,24 +9,19 @@ class Particle {
     private:
         double _x, _y, _z;
         double _mass;
-        int _charge;
     public:
         Particle(function<double()> pos_distr,
-                 function<double()> mass_distr,
-                 function<int()> charge_distr) :
+                 function<double()> mass_distr) :
             _x {pos_distr()},
             _y {pos_distr()},
             _z {pos_distr()},
-            _mass {mass_distr()},
-            _charge {charge_distr() == 0 ? -1 : 1} {};
+            _mass {mass_distr()} {};
         double x() const { return _x; }
         double y() const { return _y; }
         double z() const { return _z; }
         double mass() const {return _mass; }
-        double charge() const {return _charge; }
         void move(double dx, double dy, double dz);
         double dist(const Particle& other) const;
-        double e_force(const Particle& other) const;
 };
 
 ostream& operator<<(ostream& out, const Particle& p);
@@ -37,17 +32,14 @@ int main() {
                           ref(engine));
     auto mass_distr = bind(uniform_real_distribution<double>(0.0, 1.0),
                            ref(engine));
-    auto charge_distr = bind(uniform_int_distribution<int>(0, 1),
-                           ref(engine));
-    Particle p1(pos_distr, mass_distr, charge_distr);
-    Particle p2(pos_distr, mass_distr, charge_distr);
+    Particle p1(pos_distr, mass_distr);
+    Particle p2(pos_distr, mass_distr);
     cout << p1 << endl << p2 << endl;
     p1.move(0.5, 0.5, 0.5);
     cout << "moved: " << p1 << endl;
     cout << "x = " << p1.x() << ", y = " << p1.y() << ", z = " << p1.z()
          << endl;
     cout << "distance = " << p1.dist(p2) << endl;
-    cout << "force = " << p1.e_force(p2) << endl;
     return 0;
 }
 
@@ -67,14 +59,7 @@ double Particle::dist(const Particle& other) const {
                 sqr(_z - other.z()));
 }
 
-double Particle::e_force(const Particle& other) const {
-    const double q_e {1.6021e-19};
-    const double k_c {8.98755e9};
-    double r {dist(other)};
-    return -k_c*charge()*other.charge()*sqr(q_e/r);
-}
-
 ostream& operator<<(ostream& out, const Particle& p) {
     return out << "(" << p.x() << ", " << p.y() << ", " << p.z() << ")"
-               << ", mass = " << p.mass() << ", charge = " << p.charge();
+           << ", mass = " << p.mass();
 }
--- CPlusPlus/UserDefinedTypes/struct_particles.cpp
+++ CPlusPlus/UserDefinedTypes/struct_particles.cpp
@@ -8,15 +8,12 @@ using namespace std;
 struct Particle {
     double x, y, z;
     double mass;
-    int charge;
 };
 
 Particle init_particle(function<double()> pos_distr,
-                       function<double()> mass_distr,
-                       function<int()> charge_distr);
+                       function<double()> mass_distr);
 void move_particle(Particle& p, double dx, double dy, double dz);
 double dist(const Particle& p1, const Particle& p2);
-double e_force(const Particle& p1, const Particle& p2);
 ostream& operator<<(ostream& out, const Particle& p);
 
 
@@ -26,15 +23,12 @@ int main() {
                           ref(engine));
     auto mass_distr = bind(uniform_real_distribution<double>(0.0, 1.0),
                            ref(engine));
-    auto charge_distr = bind(uniform_int_distribution<int>(0, 1),
-                           ref(engine));
-    Particle p1 = init_particle(pos_distr, mass_distr, charge_distr);
-    Particle p2 = init_particle(pos_distr, mass_distr, charge_distr);
+    Particle p1 = init_particle(pos_distr, mass_distr);
+    Particle p2 = init_particle(pos_distr, mass_distr);
     cout << p1 << endl << p2 << endl;
     move_particle(p1, 0.5, 0.5, 0.5);
     cout << "moved: " << p1 << endl;
     cout << "distance = " << dist(p1, p2) << endl;
-    cout << "force = " << e_force(p1, p2) << endl;
     return 0;
 }
 
@@ -43,14 +37,12 @@ double sqr(double x) {
 }
 
 Particle init_particle(std::function<double()> pos_distr,
-                       std::function<double()> mass_distr,
-                       std::function<int()> charge_distr) {
+                       std::function<double()> mass_distr) {
     Particle p {
         .x = pos_distr(),
         .y = pos_distr(),
         .z = pos_distr(),
         .mass = mass_distr(),
-        .charge = charge_distr() == 0 ? -1 : 1
     };
     return p;
 }
@@ -65,15 +57,7 @@ double dist(const Particle& p1, const Particle& p2) {
     return sqrt(sqr(p1.x - p2.x) + sqr(p1.y - p2.y) + sqr(p1.z - p2.z));
 }
 
-double e_force(const Particle& p1, const Particle& p2) {
-    const double q_e {1.6021e-19};
-    const double k_c {8.98755e9};
-    double r {dist(p1, p2)};
-    return -k_c*p1.charge*p2.charge*sqr(q_e/r);
-}
-
-
 ostream& operator<<(ostream& out, const Particle& p) {
     return out << "(" << p.x << ", " << p.y << ", " << p.z << ")"
-               << ", mass = " << p.mass << ", charge = " << p.charge;
+               << ", mass = " << p.mass;
 }
