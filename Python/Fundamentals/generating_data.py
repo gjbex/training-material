0af31f572f01e811669ad97172d02588ccb6f13f
Geert Jan Bex <geertjan.bex@uhasselt.be>
2017-04-20 07:00:20 +0200

    Translated to py3k

--- Python/Fundamentals/generating_data.py
+++ Python/Fundamentals/generating_data.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 def main():
-    print 'case', 'condition', 'temperature'
+    print('case', 'condition', 'temperature')
     case_nr = 0
     for cond_nr in range(1, 4):
         for temp in [0.5*t for t in range(-1, 2)]:
             case_nr += 1
-            print case_nr, cond_nr, temp
+            print(case_nr, cond_nr, temp)
 
 if __name__ == '__main__':
     main()
