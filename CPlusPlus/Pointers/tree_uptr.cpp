cdea3521c1710ededc196a791c54e9e370e61132
Geert Jan Bex <geertjan.bex@uhasselt.be>
2017-06-01 23:46:18 +0200

    Added smart pointer example

---
+++ CPlusPlus/Pointers/tree_uptr.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <memory>
+#include "node_uptr.h"
+
+template class Node<double>;
+
+template<typename T>
+void build_tree(std::shared_ptr<Node<T>> parent, int depth);
+
+int main(int argc, char *argv[]) {
+    auto root {std::make_shared<Node<double>>(0, 1.1)};
+    build_tree<double>(root, 3);
+    root->to_str(std::cout);
+    return 0;
+}
+
+template<typename T>
+void build_tree(std::shared_ptr<Node<T>> parent, int depth) {
+    if (depth == 0)
+        return;
+    int id = parent->id();
+    T value = parent->value();
+    auto left_child {std::make_unique<Node<T>>(id + 1, value + 1)};
+    parent->set_left(left_child);
+    auto right_child {std::make_unique<Node<T>>(id + 2, value + 2)};
+    parent->set_right(right_child);
+    build_tree(parent->left(), depth - 1);
+    build_tree(parent->right(), depth - 1);
+}
