761f462526d97b4843364f50c22506c4ea0263b3
Geert Jan Bex <geertjan.bex@uhasselt.be>
2017-06-13 08:37:58 +0200

    Code cleanup

--- Accelerators/CuBLAS/gemm.cu
+++ Accelerators/CuBLAS/gemm.cu
@@ -1,29 +1,52 @@
-#include <armadillo>
 #include <functional>
 #include <iostream>
 #include <random>
 #include <sys/time.h>
-#include <cublasXt.h>
+
+#include "cl_params.h"
 
 using namespace std;
-using namespace arma;
+
+#ifndef WITHOUT_GPU_XT
+void compute_gpu_xt(int n, int nr_iters);
+#endif
+#ifndef WITHOUT_CPU
+void compute_cpu(int n, int nr_iters);
+#endif
+
+int main(int argc, char *argv[]) {
+    Params params;
+    initCl(&params);
+    parseCL(&params, &argc, &argv);
+    dumpCL(stdout, "# ", &params);
+#ifndef WITHOUT_GPU_XT
+    if (!params.skip_gpu_xt)
+        compute_gpu_xt(params.n, params.nr_iters);
+#endif
+#ifndef WITHOUT_CPU
+    if (!params.skip_cpu)
+        compute_cpu(params.n, params.nr_iters);
+#endif
+    finalizeCl(&params);
+    return 0;
+}
 
 double compute_time(struct timeval start_time, struct timeval end_time) {
     return (end_time.tv_sec - start_time.tv_sec) +
                 1.0e-6*(end_time.tv_usec - start_time.tv_usec);
 }
 
-int main(int argc, char *argv[]) {
+double compute_sum(float *ptr, int size) {
     float sum {0.0};
+    for (int i = 0; i < size; i++)
+        sum += ptr[i];
+    return sum;
+}
+
+#ifndef WITHOUT_GPU_XT
+#include <cublasXt.h>
+void compute_gpu_xt(int n, int nr_iters) {
     struct timeval start_time, end_time;
-    int n = 500;
-    if (argc > 1)
-        n = atoi(argv[1]);
-    int nr_iters = 10;
-    if (argc > 2)
-        nr_iters = atoi(argv[2]);
-    cout << n << "x" << n << " matrices, " << nr_iters << " iterations"
-         << endl;
     float *a {new float[n*n]};
     float *b {new float[n*n]};
     float *c {new float[n*n]};
@@ -31,7 +54,6 @@ int main(int argc, char *argv[]) {
         cerr << "#error: can't allocate memory" << endl;
         return 1;
     }
-
     for (int i = 0; i < n*n; i++) {
         a[i] = i + 1.0;
         b[i] = i + 10.0;
@@ -39,7 +61,6 @@ int main(int argc, char *argv[]) {
     }
     float alpha {1.0};
     float beta {1.0};
-    cout << "host matrices allocated and initialized" << endl;
     cublasXtHandle_t handle;
     cublasStatus_t status = cublasXtCreate(&handle);
     const int nr_devices {2};
@@ -53,45 +74,46 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < nr_iters; i++) {
         status = cublasXtSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N,
                                n, n, n, &alpha, a, n, b, n, &beta, c, n);
+        if (status != CUBLAS_STATUS_SUCCESS) {
+            cerr << "# error: cublasXtSgemm returned: " << status << endl;
+            return 1;
+        }
     }
     gettimeofday(&end_time, NULL);
-    cout << "GPU computation time = " << compute_time(start_time, end_time)
-         << endl;
-    sum = 0.0;
-    for (int i = 0; i < n*n; i++)
-        sum += c[i];
-    cout << "GPU sum = " << sum << endl;
+    cout << "GPU_XT computation time = "
+         << compute_time(start_time, end_time) << endl;
+         << "GPU_XT sum = " << compute_sum(c, n*n) << endl;
     delete a;
     delete b;
     delete c;
     cublasXtDestroy(handle);
+}
+#endif
 
+#ifndef WITHOUT_CPU
+#include <armadillo>
+using namespace arma;
+void compute_cpu(int n, int nr_iters) {
+    struct timeval start_time, end_time;
     auto A = Mat<float>(n, n);
     auto B = Mat<float>(n, n);
-    auto C = Mat<float>(n, n);
+    auto C = zero<Mat<float>>(n, n);
     int k = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++) {
             A(i, j) = k + 1.0;
             B(i, j) = k + 10.0;
-            C(i, j) = 0.0;
             k++;
         }
     }
-    cout << "CPU matrices allocated and initialized" << endl;
     gettimeofday(&start_time, NULL);
     for (int i = 0; i < nr_iters; i++) {
         C += A*B;
     }
     gettimeofday(&end_time, NULL);
-    cout << "CPU computation time = " << compute_time(start_time, end_time)
+    cout << "CPU computation time = "
+         << compute_time(start_time, end_time) << endl;
+         << "CPU sum = " << compute_sum(C.memptr(), C.n_rows*C.n_cols)
          << endl;
-    sum = 0.0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            sum += C(i, j);
-        }
-    }
-    cout << "CPU sum = " << sum << endl;
-    return 0;
 }
+#endif
