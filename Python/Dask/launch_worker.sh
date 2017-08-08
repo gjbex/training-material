f95fa0b179c215f3d607ed04be5b9d3d9d19ef3c
Geert Jan Bex <geertjan.bex@uhasselt.be>
2017-08-08 11:10:41 +0200

    Intercepted conda activate path setting feedback

--- Python/Dask/launch_worker.sh
+++ Python/Dask/launch_worker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -l
 
 source "${VSC_DATA}/miniconda3/setenv.sh"
-source activate science
+source activate science 2> /dev/null
+if [ $? -ne 0 ]
+then
+    (>&2 echo '### error: conda environment not sourced correctly' )
+fi
 
-nohup dask-worker $1 &> "worker-$(hostname).log" &
+nohup dask-worker $1 &> "worker-$(hostname)-${PBS_JOBID}.log" &
