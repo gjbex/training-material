9aed807afdd22efe136d6f15bf1b42a377ee4dff
Geert Jan Bex <geertjan.bex@uhasselt.be>
2017-08-10 15:04:35 +0200

    Simplify code for presentation

--- Python/Numba/JuliaSet/julia_numba.py
+++ Python/Numba/JuliaSet/julia_numba.py
@@ -4,15 +4,6 @@ from numba import jit
 
 
 @jit
-def julia_point(z, max_norm, max_iters):
-    iterations = 0
-    while (iterations <= max_iters and
-           z.real*z.real + z.imag*z.imag <= max_norm*max_norm):
-        z = z**2 - 0.622772 + 0.42193j
-        iterations += 1
-    return iterations
-
-
 def julia_set(domain, iterations, max_norm, max_iters):
     '''Compute the Julia set on a complex domain.
 
@@ -22,5 +13,8 @@ def julia_set(domain, iterations, max_norm, max_iters):
     max_norm -- maximum complex norm to iterate to.
     max_iters -- maximum number of iterations
     '''
-    for i in range(len(domain)):
-        iterations[i] = julia_point(domain[i], max_norm, max_iters)
+    for i, z in enumerate(domain):
+        while (iterations[i] <= max_iters and
+               z.real*z.real + z.imag*z.imag <= max_norm*max_norm):
+            z = z**2 - 0.622772 + 0.42193j
+            iterations[i] += 1
--- Python/Numba/JuliaSet/julia_numba_eager.py
+++ Python/Numba/JuliaSet/julia_numba_eager.py
@@ -1,18 +1,9 @@
 '''Module containing function for computing Julia sets'''
 
-from numba import jit, int32, float64, complex128
+from numba import jit, void, int32, float64, complex128
 
 
-@jit(int32(complex128, float64, int32))
-def julia_point(z, max_norm, max_iters):
-    iterations = 0
-    while (iterations <= max_iters and
-           z.real*z.real + z.imag*z.imag <= max_norm*max_norm):
-        z = z**2 - 0.622772 + 0.42193j
-        iterations += 1
-    return iterations
-
-
+@jit(void(complex128[:], int32[:], float64, int32))
 def julia_set(domain, iterations, max_norm, max_iters):
     '''Compute the Julia set on a complex domain.
 
@@ -23,4 +14,7 @@ def julia_set(domain, iterations, max_norm, max_iters):
     max_iters -- maximum number of iterations
     '''
     for i, z in enumerate(domain):
-        iterations[i] = julia_point(z, max_norm, max_iters)
+        while (iterations[i] <= max_iters and
+               z.real*z.real + z.imag*z.imag <= max_norm*max_norm):
+            z = z**2 - 0.622772 + 0.42193j
+            iterations[i] += 1
