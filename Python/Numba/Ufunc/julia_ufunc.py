2567f0c74d22d9f378529b9e88c4cccf435f6655
Geert Jan Bex <geertjan.bex@uhasselt.be>
2017-08-11 15:02:20 +0200

    Fix performance issue

--- Python/Numba/Ufunc/julia_ufunc.py
+++ Python/Numba/Ufunc/julia_ufunc.py
@@ -8,7 +8,7 @@ from numba import guvectorize, void, complex128, int32, float64
 def julia_set(domain, max_norm, max_iters, iterations):
     for i, z in enumerate(domain):
         iterations[i] = 0
-        while (iterations[i] <= max_iters and
-               z.real*z.real + z.imag*z.imag <= max_norm*max_norm):
+        while (iterations[i] <= max_iters[0] and
+               z.real*z.real + z.imag*z.imag <= max_norm[0]*max_norm[0]):
             z = z**2 - 0.622772 + 0.42193j
             iterations[i] += 1
