fe7acc3de2d336fa7c4a43ed8491febec928d43b
Geert Jan Bex <geertjan.bex@uhasselt.be>
2017-09-12 07:47:59 +0200

    Clean up code

--- Debugging/Mpi/buffer_overlap.f90
+++ Debugging/Mpi/buffer_overlap.f90
@@ -1,6 +1,7 @@
 program message_size
 use :: mpi
 implicit none
+integer, parameter :: root = 0
 integer, parameter :: buff_size = 5
 integer, dimension(buff_size) :: buff
 integer :: ierr, rank, i
@@ -11,10 +12,10 @@ do i = 1, buff_size
     buff(i) = i
 end do
 
-call MPI_Reduce(buff, buff, buff_size, MPI_INTEGER, MPI_SUM, 0, &
+call MPI_Reduce(buff, buff, buff_size, MPI_INTEGER, MPI_SUM, root, &
                 MPI_COMM_WORLD, ierr)
 
-if (rank == 0) then
+if (rank == root) then
     do i = 1, buff_size
         print '(I15)', buff(i)
     end do
