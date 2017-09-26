cffa2c2531f845750b985ee2b53d67fbc9d3ab0b
Geert Jan Bex <geertjan.bex@uhasselt.be>
2017-09-26 08:10:16 +0200

    Modify output format for slides

--- Debugging/ControlFlow/init_proc_vars.f90
+++ Debugging/ControlFlow/init_proc_vars.f90
@@ -2,20 +2,11 @@ program init_proc_vars
     implicit none
     integer, parameter :: nr_elems = 5, nr_arrays = 5
     integer, dimension(nr_elems) :: a
-    integer, dimension(nr_arrays) :: right, wrong
-    integer :: i
+    integer :: i, nr_ones
     do i = 1, nr_arrays
         call init_array(a)
-        wrong(i) = count_one_wrong(a)
-        right(i) = count_one_right(a)
-    end do
-    print '(A)', 'wrong:'
-    do i = 1, nr_arrays
-        print '(I5, I10)', i, wrong(i)
-    end do
-    print '(A)', 'right:'
-    do i = 1, nr_arrays
-        print '(I5, I10)', i, right(i)
+        nr_ones = count_one_wrong(a)
+        print '(I5, I4, A, I1)', i, nr_ones, '/', size(a)
     end do
 
 contains
@@ -37,27 +28,16 @@ contains
         end do
     end subroutine init_array
 
-    function count_one_wrong(a) result(count)
+    function count_one_wrong(a) result(result)
         implicit none
         integer, dimension(:), intent(in) :: a
-        integer :: count
-        integer :: c = 0
+        integer :: result
+        integer :: count = 0
         integer :: i
-        do i = 1, size(a)
-            if (a(i) > 0) c = c + 1
-        end do
-        count = c
-    end function count_one_wrong
-
-    function count_one_right(a) result(count)
-        implicit none
-        integer, dimension(:), intent(in) :: a
-        integer :: count
-        integer :: i
-        count = 0
         do i = 1, size(a)
             if (a(i) > 0) count = count + 1
         end do
-    end function count_one_right
+        result = count
+    end function count_one_wrong
 
 end program init_proc_vars
