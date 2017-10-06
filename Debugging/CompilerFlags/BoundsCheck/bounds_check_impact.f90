c9c033499655194b5fc02c2cebe9bfd4fddd50a2
Geert Jan Bex <geertjan.bex@uhasselt.be>
2017-10-06 07:35:05 +0200

    Increased run time to reduce noise

--- Debugging/CompilerFlags/BoundsCheck/bounds_check_impact.f90
+++ Debugging/CompilerFlags/BoundsCheck/bounds_check_impact.f90
@@ -1,6 +1,6 @@
 program array_bounds_check
     implicit none
-    integer, parameter :: n = 1000000, nr_searches = 1000
+    integer, parameter :: n = 1000000, nr_searches = 5000
     integer, dimension(n) :: x
     integer :: i, search_nr, nr_found
     do i = 1, n
