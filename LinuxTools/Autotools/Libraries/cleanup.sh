88003db56de1f2e4ce75cceabbfca3cd8659214b
Geert Jan Bex <geertjan.bex@uhasselt.be>
2017-10-31 12:27:21 +0100

    Add removal of hidden dependency directory

--- LinuxTools/Autotools/Libraries/cleanup.sh
+++ LinuxTools/Autotools/Libraries/cleanup.sh
@@ -2,7 +2,7 @@
 
 ITEMS="aclocal.m4 autom4te.cache config.log config.status configure \
         compile depcomp install-sh Makefile Makefile.in missing \
-        src/Makefile src/Makefile.in"
+        src/Makefile src/Makefile.in src/.deps"
 
 make clean
 
