89875eee1c3089b6e227bbcbc2f9ad0191c4dfa1
Geert Jan Bex <geertjan.bex@uhasselt.be>
2017-10-31 19:24:00 +0100

    Make clean optional

--- LinuxTools/Autotools/Simple/cleanup.sh
+++ LinuxTools/Autotools/Simple/cleanup.sh
@@ -4,6 +4,11 @@ ITEMS="aclocal.m4 autom4te.cache config.log config.status configure \
         depcomp install-sh Makefile Makefile.in missing \
         src/Makefile src/Makefile.in"
 
+if [ -e Makefile ]
+then
+    make clean
+fi
+
 for file in ${ITEMS}
 do
     rm -rf "${file}"
