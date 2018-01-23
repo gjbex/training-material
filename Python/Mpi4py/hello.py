0bcab388fc48730f43e78b5e5c160a5070fc7c04
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-01-23 17:37:22 +0100

    Add affinity info

--- Python/Mpi4py/hello.py
+++ Python/Mpi4py/hello.py
@@ -1,6 +1,8 @@
 #/usr/bin/env python
 
 from mpi4py import MPI
+import os
+import psutil
 import socket
 
 if __name__ == '__main__':
@@ -8,4 +10,11 @@ if __name__ == '__main__':
     rank = comm.Get_rank()
     size = comm.Get_size()
     host = socket.gethostname()
-    print('rank {0:02d} of {1:02d} on {2:s}'.format(rank, size, host))
+    pid = os.getpid()
+    msg = 'rank {0:02d} of {1:02d} as process {2:05d} on {3:s}'
+    print(msg.format(rank, size, pid, host))
+    process = psutil.Process()
+    nr_cores = psutil.cpu_count()
+    cores = process.cpu_affinity()
+    msg = 'rank {0:02d} running on core(s) {1:s} of {2:d}'
+    print(msg.format(rank, '.'.join(str(c) for c in cores), nr_cores))
