d8a74042cbf9ddd321e7da7e3ef6a357f4e1d250
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-03-20 08:31:37 +0100

    Make problem a bit more obvious

--- Debugging/CompilerFlags/UndefMacroVar/undef_macro_var.c
+++ Debugging/CompilerFlags/UndefMacroVar/undef_macro_var.c
@@ -5,13 +5,18 @@
 
 int main(void) {
     char fmt[80];
+    float x = 0.0f;
+    {
 #if PREC == 32
-    float x = 2.0f;
-    strcpy(fmt, "sqrt(%.7f) = %.7f\n");
-#else
-    double x = 2.0;
-    strcpy(fmt, "sqrt(%.15lf) = %.15lf\n");
+        float x = 2.0f;
+        strcpy(fmt, "sqrt(%.7f) = %.7f\n");
+        printf("single precision\n");
+#elif PREC == 64
+        double x = 2.0;
+        strcpy(fmt, "sqrt(%.15lf) = %.15lf\n");
+        printf("double precision\n");
 #endif
-    printf(fmt, x, sqrt(x));
+        printf(fmt, x, sqrt(x));
+    }
     return EXIT_SUCCESS;
 }
