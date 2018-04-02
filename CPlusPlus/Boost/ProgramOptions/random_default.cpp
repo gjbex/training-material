cf2f9e54dc1c0fbd06140d349ab35a8322e60e8b
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-04-02 18:31:09 +0200

    Add extra options

--- CPlusPlus/Boost/ProgramOptions/random_default.cpp
+++ CPlusPlus/Boost/ProgramOptions/random_default.cpp
@@ -1,17 +1,28 @@
 #include <boost/program_options.hpp>
 #include <iostream>
+#include <memory>
 #include <random>
 
 namespace po = boost::program_options;
 
 int main(int argc, char* argv[]) {
     const int default_n {1};
+    const double default_a {0.0};
+    const double default_b {1.0};
     int n;
+    double a, b;
+    size_t seed;
+
     po::options_description desc("Allowed options");
     desc.add_options()
         ("help", "produce help message")
         ("n", po::value<int>(&n)->default_value(default_n),
          "number of random values to generate")
+        ("a", po::value<double>(&a)->default_value(default_a),
+         "minimum value")
+        ("b", po::value<double>(&b)->default_value(default_b),
+         "maximum value")
+        ("seed", po::value<size_t>(&seed), "seed to use")
     ;
 
     po::variables_map vm;        
@@ -23,9 +34,12 @@ int main(int argc, char* argv[]) {
         return 0;
     }
 
-    std::random_device device;
-    std::mt19937 engine(device());
-    std::uniform_real_distribution<double> distr(0.0, 1.0);
+    if (!vm.count("seed")) {
+        std::random_device device;
+        seed = device();
+    }
+    std::mt19937 engine(seed);
+    std::uniform_real_distribution<double> distr(a, b);
     for (int i = 0; i < n; ++i)
         std::cout << distr(engine) << std::endl;
 
