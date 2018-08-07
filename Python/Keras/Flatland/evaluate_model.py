35412bef527c3306c8c7e00cfe2ea230633b5565
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-08-07 22:09:57 +0200

    Add verbosity option

--- Python/Keras/Flatland/evaluate_model.py
+++ Python/Keras/Flatland/evaluate_model.py
@@ -12,13 +12,17 @@ if __name__ == '__main__':
     arg_parser.add_argument('--test', help='HDF5 file with test data')
     arg_parser.add_argument('model_file', help='HDF5 file containing '
                                                'the model')
+    arg_parser.add_argument('--verbose', type=int, default=1,
+                            help='verbosity level of evaluation')
     options = arg_parser.parse_args()
     model = load_model(options.model_file)
     if options.train:
         x_train, y_train = process_data(options.train)
-        loss, accuracy = model.evaluate(x_train, y_train)
+        loss, accuracy = model.evaluate(x_train, y_train,
+                                        verbose=options.verbose)
         print(f'training: loss = {loss:.4f}, accuracy = {accuracy:.4f}')
     if options.test:
         x_test, y_test = process_data(options.test)
-        loss, accuracy = model.evaluate(x_test, y_test)
+        loss, accuracy = model.evaluate(x_test, y_test,
+                                        verbose=options.verbose)
         print(f'test: loss = {loss:.4f}, accuracy = {accuracy:.4f}')
