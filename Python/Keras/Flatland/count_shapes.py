190b46f49c452b41c069eea851956d9e7cfa8afc
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-08-07 22:22:17 +0200

    Simplify code

--- Python/Keras/Flatland/count_shapes.py
+++ Python/Keras/Flatland/count_shapes.py
@@ -1,44 +1,15 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
-import h5py
-from keras import backend as K
 from keras.layers import (Activation, Conv2D, Dense, Dropout,
                           Flatten, LeakyReLU)
 from keras.models import Sequential
 from keras.optimizers import Adam
 import numpy as np
-from pathlib import Path
-from sklearn.model_selection import train_test_split
 
+from data_utils import get_data
+from file_utils import change_path_suffix
 from history_utils import store_history
-
-
-def compute_input_shape(x_train):
-    img_channels = 1
-    img_rows, img_cols = x_train.shape[1], x_train.shape[2]
-    if K.image_data_format() == 'channels_first':
-        return (img_channels, img_rows, img_cols)
-    else:  # channel_last
-        return (img_rows, img_cols, img_channels)
-
-
-def process_data(data_file):
-    with h5py.File(data_file, 'r') as h5_file:
-        x_data = np.array(h5_file['x_values'])
-        y_data = np.array(h5_file['y_values'])
-    shape_ord = compute_input_shape(x_data)
-    x_data = x_data.reshape((x_data.shape[0], ) + shape_ord)
-    x_data = x_data.astype(np.float32)/255.0
-    return x_data, y_data
-
-
-def get_data(training_file, test_file):
-    x_train, y_train = process_data(training_file)
-    x_test, y_test = process_data(test_file)
-    np.random.seed(1234)
-    x_train, x_val, y_train, y_val = train_test_split(x_train, y_train)
-    return x_train, x_val, x_test, y_train, y_val, y_test
 
 
 def config_model(input_shape, output_shape):
@@ -65,13 +36,6 @@ def config_model(input_shape, output_shape):
     model.compile(loss='mean_squared_error', optimizer=Adam(),
                   metrics=['accuracy'])
     return model
-
-
-def change_path_suffix(path_str, suffix_str):
-    path = Path(path_str)
-    dir_path = path.parent
-    file_base = path.stem
-    return str(dir_path / (file_base + suffix_str))
 
 
 if __name__ == '__main__':
