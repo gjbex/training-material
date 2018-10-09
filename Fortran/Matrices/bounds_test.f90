e338f4c6eb3e8bb2b5c961b0add918cacf14c014
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-10-09 06:56:35 +0200

    Improve code formatting

--- Fortran/Matrices/bounds_test.f90
+++ Fortran/Matrices/bounds_test.f90
@@ -2,7 +2,8 @@ program bounds_test
     implicit none
     integer, dimension(-2:2) :: values = [ 1, 2, 3, 4, 5 ]
 
-    print '("program: ", I0, " -> ", I0)', lbound(values), ubound(values)
+    print '("program: ", I0, " -> ", I0)', &
+        lbound(values), ubound(values)
     call print_array(values)
 
 contains
@@ -12,7 +13,8 @@ contains
         integer, dimension(:), intent(in) :: values
         integer :: i
 
-        print '("subroutine: ", I0, " -> ", I0)', lbound(values), ubound(values)
+        print '("subroutine: ", I0, " -> ", I0)', &
+            lbound(values), ubound(values)
         do i = 1, size(values)
             print '(I0)', values(i)
         end do
