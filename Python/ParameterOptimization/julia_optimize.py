eae6b4ba07837aba2a5aadd28bd0b4b788074302
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-12-23 19:05:05 +0100

    Fix various bugs

--- Python/ParameterOptimization/julia_optimize.py
+++ Python/ParameterOptimization/julia_optimize.py
@@ -14,9 +14,9 @@ def function(params):
     chunk = int(chunk)
     # ppn ranges from 0 to 35 (inclusive)
     ppn = 1 + int(ppn)
-    omp_env = f'OMP_SCHEDULE={schedule},{int(chunk)},'
-              f'OMP_NUM_THREADS={ppn}'
-    cmd = ['qsub', '-l', f'nodes=1:ppn={ppn}',
+    omp_env = (f'schedule={schedule},chunk={int(chunk)},' +
+               f'OMP_NUM_THREADS={ppn}')
+    cmd = ['qsub', '-l', f'nodes=1:ppn={ppn}:haswell',
            '-v', omp_env, 'julia.pbs']
     process = subprocess.run(cmd, stdout=subprocess.PIPE,
                              encoding='utf8')
@@ -26,7 +26,7 @@ def function(params):
     while not output_file.exists():
         time.sleep(3)
     print(f'### info: job {job_id} finished', file=sys.stderr)
-    with open(f'julia.time{job_id}', 'r') as time_file:
+    with open(f'julia.pbs.time{job_id}', 'r') as time_file:
         runtime = float(time_file.readline())
     return {
         'loss': runtime, 'schedule': schedule, 'chunk': chunk,
@@ -35,14 +35,14 @@ def function(params):
     }
 
 
-def optimize(max_evals):
+def optimize(max_evals, max_ppn):
     space = hp.choice('schedule', [
         ('static', hp.qloguniform('chunk_s', 2, 11, 10),
-         hp.randint('ppn_s', 36)),
+         hp.randint('ppn_s', max_ppn)),
         ('dynamic', hp.qloguniform('chunk_d', 2, 11, 10),
-         hp.randint('ppn_d', 36)),
+         hp.randint('ppn_d', max_ppn)),
         ('guided', hp.qloguniform('chunk_g', 2, 11, 10),
-         hp.randint('ppn_g', 36)),
+         hp.randint('ppn_g', max_ppn)),
     ])
     trials = Trials()
     best = fmin(function, space=space, algo=tpe.suggest,
@@ -53,12 +53,14 @@ def optimize(max_evals):
 def main():
     arg_parser = ArgumentParser(description='optimize external '
                                             'process')
+    arg_parser.add_argument('--max-ppn', type=int, default=20,
+                            help='maximum number of cores to use')
     arg_parser.add_argument('--max-evals', type=int,
                             default=100, help='maximum evals')
     arg_parser.add_argument('--trials', action='store_true',
                             help='display trials')
     options = arg_parser.parse_args()
-    best, trials = optimize(options.max_evals)
+    best, trials = optimize(options.max_evals, options.max_ppn)
     if options.trials:
         print('x y value')
         for trial in trials.results:
