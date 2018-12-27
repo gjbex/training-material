1936368ee36a456e0450c9cfa17c1f24106270ea
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-12-27 17:59:03 +0100

    Add rethrow of exception

--- CPlusPlus/Modularity/memory_leak.cpp
+++ CPlusPlus/Modularity/memory_leak.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <limits>
 #include <stdexcept>
 #include <vector>
 
 const int ERR_ARGUMENTS = 1;
 const int ERR_DOMAIN = 2;
+const int ERR_OVERFLOW = 3;
 
 using Vector = std::vector<int> *;
 
@@ -25,6 +27,9 @@ int main(int argc, char *argv[]) {
     } catch (std::domain_error& e) {
         std::cerr << "### error: " << e.what() << std::endl;
         return ERR_DOMAIN;
+    } catch (std::overflow_error& e) {
+        std::cerr << "### error: overflow, " << e.what() << std::endl;
+        return ERR_OVERFLOW;
     }
     return 0;
 }
@@ -33,16 +38,26 @@ int fac(int n) {
     if (n < 0) {
         throw std::domain_error {"negative argument for fac"};
     } else {
+        const int max_int = std::numeric_limits<int>::max();
         int value = 1;
         for (int i = 2; i <= n; ++i)
-            value *= i;
+            if (value < max_int/i)
+                value *= i;
+            else
+                throw std::overflow_error {std::to_string(n) +
+                                           " caused overflow"};
         return value;
     }
 }
 
 Vector fac_range(int min_value, int max_value) {
     Vector values = new std::vector<int>();
-    for (int i = min_value; i < max_value; ++i)
-        values->push_back(fac(i));
+    try {
+        for (int i = min_value; i < max_value; ++i)
+            values->push_back(fac(i));
+    } catch(std::exception& e) {
+        std::cerr << "# warning: something went wrong in fac_range" << std::endl;
+        throw;
+    }
     return values;
 }
