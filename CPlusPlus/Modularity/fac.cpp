db25d163802110f865cb7c02957821dcac31a91d
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-12-27 13:31:18 +0100

    Catch exceptions by reference; replace exception by more appropriate type

--- CPlusPlus/Modularity/fac.cpp
+++ CPlusPlus/Modularity/fac.cpp
@@ -16,18 +16,18 @@ int main(int argc, char *argv[]) {
     int n = 0;
     try {
         n = std::stoi(argv[1]);
-    } catch(std::invalid_argument) {
+    } catch(std::invalid_argument&) {
         std::cerr << "# error: '" << argv[1] << "' can not be converted to int"
              << std::endl;
         return EXIT_ARG_TYPE;
-    } catch(std::out_of_range) {
+    } catch(std::out_of_range&) {
         std::cerr << "# error: " << argv[1]  << " is out of range" << std::endl;
         return EXIT_OUT_OF_RANGE;
     }
     try {
         std::cout << fac(n) << std::endl;
-    } catch(std::invalid_argument e) {
-        std::cerr << "# error: invalid argument, " << e.what() << std::endl;
+    } catch(std::domain_error& e) {
+        std::cerr << "# error: domain error, " << e.what() << std::endl;
         return EXIT_NEG_ARG;
     }
     return 0;
@@ -37,7 +37,7 @@ int fac(int n) {
     if (n < 0) {
         std::string msg {"fac argument "};
         msg += std::to_string(n) + ", must be positive";
-        throw std::invalid_argument {msg};
+        throw std::domain_error {msg};
     } else {
         int result = 1;
         for (int i = 2; i <= n; i++) {
