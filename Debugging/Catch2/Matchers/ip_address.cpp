53737e3741d56cbecdfea95354bb7145e22e7a51
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-12-29 17:11:29 +0100

    Add test for bytes of address

--- Debugging/Catch2/Matchers/ip_address.cpp
+++ Debugging/Catch2/Matchers/ip_address.cpp
@@ -18,7 +18,17 @@ TEST_CASE("random IP4 address generation") {
     std::mt19937::result_type seed {1234};
     auto distr = std::bind(std::uniform_int_distribution<int>(0, 255),
                            std::mt19937(seed));
-    for (int i = 0; i < 10; ++i)
-        REQUIRE_THAT( generate_ip4_address(distr),
-                      Matches(R"(^\d{1,3}(?:\.\d{1,3}){3}$)") );
+    for (int i = 0; i < 10; ++i) {
+        auto ip_address = generate_ip4_address(distr);
+        REQUIRE_THAT( ip_address, Matches(R"(^\d{1,3}(?:\.\d{1,3}){3}$)") );
+        size_t pos {0};
+        size_t old_pos {pos};
+        while ((pos = ip_address.find(".", old_pos)) &&
+               pos != std::string::npos) {
+            auto byte {std::stoi(ip_address.substr(old_pos, pos))};
+            REQUIRE( 0 <= byte );
+            REQUIRE( byte < 256 );
+            old_pos = pos + 1;
+        }
+    }
 }
