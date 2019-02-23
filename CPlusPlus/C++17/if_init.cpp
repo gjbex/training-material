742f9e838587c6175f83effb25d02e63245e73ed
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-02-23 22:52:04 +0100

    Remove unused argc and argv
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- CPlusPlus/C++17/if_init.cpp
+++ CPlusPlus/C++17/if_init.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
-int main(int argc __attribute__((unused)),
-         char *argv[] __attribute__((unused))) {
+int main() {
     if (std::string str; std::cin >> str) {
         std::cout << "read '" << str << "'" << std::endl;
     }
