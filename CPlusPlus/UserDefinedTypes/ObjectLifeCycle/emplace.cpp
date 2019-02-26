0fb1833a4687b56a225543dce1f9e1b9d29c1a57
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-02-26 17:23:29 +0100

    Fix declaration of copy constructor and assignment operator
    Why:
    
    * Copy constructor and assignment operator should have `const T&&` argument.
    
    This change addresses the need by:
    
    *

--- CPlusPlus/UserDefinedTypes/ObjectLifeCycle/emplace.cpp
+++ CPlusPlus/UserDefinedTypes/ObjectLifeCycle/emplace.cpp
@@ -3,15 +3,18 @@
 
 struct S {
     S() { puts("S()"); }
-    S(S&) { puts("S(&)"); }
+    S(const S&) { puts("S(&)"); }
     S(S&&) { puts("S(&&)"); }
     ~S() { puts("~S()"); }
 };
 
-int main() {
+int main(int argc, char *[]) {
+    const size_t reserved {5};
     std::vector<S> v;
-    v.reserve(10);
-    for (int i = 0; i < 5; i++)
+    v.reserve(reserved);
+    for (size_t i = 0; i < reserved; i++)
+        v.emplace_back();
+    if (argc > 1)
         v.emplace_back();
     return 0;
 }
