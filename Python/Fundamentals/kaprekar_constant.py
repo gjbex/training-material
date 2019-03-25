fc935c317796e80b7c9a47b9b535893e08b05aee
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-03-25 21:45:40 +0100

    Add validation functions; simplify implementation
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- Python/Fundamentals/kaprekar_constant.py
+++ Python/Fundamentals/kaprekar_constant.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
 import click
+import sys
 
 CONTEXT_SETTINGS = {
-        'help_option_names': ('-h', '--help')
+    'help_option_names': ('-h', '--help'),
 }
+
+
+def check_valid(n):
+    if n < 1000 or n > 9999:
+        print('Error: argument must have four digits', file=sys.stderr)
+        sys.exit(1)
+    if len(set(str(n))) < 2:
+        print('Error: argument must have at least two distinct digits',
+              file=sys.stderr)
+        sys.exit(2)
 
 
 def compute_sorted(n, reverse=True):
@@ -12,18 +23,20 @@ def compute_sorted(n, reverse=True):
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.argument('n', type=int, default=1112)
+@click.argument('n', type=int)
 def compute_kaprekar(n):
+    '''Given a four digit number with at least two distinct digits,
+    this script computes the Kaprekar constant.
+    '''
+    check_valid(n)
     print(n, end='')
-    small = compute_sorted(n, reverse=False)
-    prev_small = 0
     while True:
+        prev_n = n
+        small = compute_sorted(n, reverse=False)
         large = compute_sorted(n, reverse=True)
         n = large - small
-        prev_small = small
-        small = compute_sorted(n, reverse=False)
-        if small != prev_small:
-            print(' ->', n, end='')
+        if n != prev_n:
+            print(f' -> {n}', end='')
         else:
             break
     print()
