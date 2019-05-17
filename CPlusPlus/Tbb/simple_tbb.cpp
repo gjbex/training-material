a7618f0ede02631d676576a88660313c51e5b167
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-05-17 16:53:08 +0200

    Eliminate use of std namespace
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- CPlusPlus/Tbb/simple_tbb.cpp
+++ CPlusPlus/Tbb/simple_tbb.cpp
@@ -3,35 +3,33 @@
 #include <tbb/tbb.h>
 #include <valarray>
 
-using namespace std;
-
-void print_values(const valarray<int>& v) {
+void print_values(const std::valarray<int>& v) {
     tbb::parallel_for(tbb::blocked_range<size_t>(0, v.size()),
             [&] (const tbb::blocked_range<size_t>& r) {
-                for (size_t i = r.begin(); i < r.end(); i++) {
-                    string line;
-                    stringstream stream(line);
-                    stream << i << ": " << v[i] << endl;
-                    cout << stream.str();
+                for (std::size_t i = r.begin(); i < r.end(); i++) {
+                    std::string line;
+                    std::stringstream stream(line);
+                    stream << i << ": " << v[i] << std::endl;
+                    std::cout << stream.str();
                 }
             });
 }
 
-valarray<int> init_values(size_t n) {
-    valarray<int> v(n);
+std::valarray<int> init_values(size_t n) {
+    std::valarray<int> v(n);
     tbb::parallel_for(tbb::blocked_range<size_t>(0, n),
             [&] (const tbb::blocked_range<size_t>& r) {
-                for (size_t i = r.begin(); i < r.end(); i++)
+                for (std::size_t i = r.begin(); i < r.end(); i++)
                     v[i] = i*i;
             });
     return v;
 }
 
 int main(int argc, char *argv[]) {
-    size_t nv {100};
+    std::size_t nv {100};
     if (argc > 1)
-        nv = stoi(argv[1]);
-    valarray<int> v = init_values(nv);
+        nv = std::stoi(argv[1]);
+    std::valarray<int> v = init_values(nv);
     print_values(v);
     return 0;
 }
