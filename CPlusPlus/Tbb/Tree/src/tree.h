2a3f79fdcba4aef05ec42b5d9dbd0b6d5f5c089b
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-05-27 08:47:23 +0200

    Add tree implementation to expreiment with TBB
    Why:
    
    * Can be used to illustrate TBB parallelisation on recursive data types.
    
    This change addresses the need by:
    
    *

---
+++ CPlusPlus/Tbb/Tree/src/tree.h
@@ -0,0 +1,30 @@
+#ifndef TREE_HDR
+#define TREE_HDR
+
+#include <iostream>
+#include <memory>
+
+template<typename T>
+class Node {
+    private:
+        T data_;
+        std::unique_ptr<Node<T>> left_child_;
+        std::unique_ptr<Node<T>> right_child_;
+    public:
+        Node(T data) : data_ {data}, left_child_ {nullptr}, right_child_ {nullptr} {};
+        T& operator()() { return data_; }
+        Node<T>* left() { return left_child_.get(); }
+        Node<T>* right() { return right_child_.get(); }
+        void set_left(std::unique_ptr<Node<T>> child) {
+            left_child_ = std::move(child);
+        }
+        void set_right(std::unique_ptr<Node<T>> child) {
+            right_child_ = std::move(child);
+        }
+        friend std::ostream& operator<<(std::ostream& out, const Node<T>& node) {
+            out << node.data_;
+            return out;
+        }
+};
+
+#endif
