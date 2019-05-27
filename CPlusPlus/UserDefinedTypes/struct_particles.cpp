442a9d00ce422183cede0958e0ab3522b874eba6
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-05-27 12:07:22 +0200

    Clean up code a bit
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- CPlusPlus/UserDefinedTypes/struct_particles.cpp
+++ CPlusPlus/UserDefinedTypes/struct_particles.cpp
@@ -3,47 +3,40 @@
 #include <iostream>
 #include <random>
 
-using namespace std;
-
 struct Particle {
     double x, y, z;
     double mass;
 };
 
-Particle init_particle(function<double()> pos_distr,
-                       function<double()> mass_distr);
+Particle init_particle(std::function<double()> pos_distr,
+                       std::function<double()> mass_distr);
 void move_particle(Particle& p, double dx, double dy, double dz);
 double dist(const Particle& p1, const Particle& p2);
-ostream& operator<<(ostream& out, const Particle& p);
+std::ostream& operator<<(std::ostream& out, const Particle& p);
 
 
 int main() {
-    auto engine {mt19937_64(1234)};
-    auto pos_distr = bind(uniform_real_distribution<double>(-1.0, 1.0),
-                          ref(engine));
-    auto mass_distr = bind(uniform_real_distribution<double>(0.0, 1.0),
-                           ref(engine));
+    std::mt19937_64 engine(1234);
+    auto pos_distr = std::bind(std::uniform_real_distribution<double>(-1.0, 1.0),
+                               std::ref(engine));
+    auto mass_distr = std::bind(std::uniform_real_distribution<double>(0.0, 1.0),
+                                std::ref(engine));
     Particle p1 = init_particle(pos_distr, mass_distr);
     Particle p2 = init_particle(pos_distr, mass_distr);
-    cout << p1 << endl << p2 << endl;
+    std::cout << p1 << std::endl << p2 << std::endl;
     move_particle(p1, 0.5, 0.5, 0.5);
-    cout << "moved: " << p1 << endl;
-    cout << "distance = " << dist(p1, p2) << endl;
+    std::cout << "moved: " << p1 << std::endl;
+    std::cout << "distance = " << dist(p1, p2) << std::endl;
     return 0;
 }
 
-double sqr(double x) {
+inline double sqr(double x) {
     return x*x;
 }
 
 Particle init_particle(std::function<double()> pos_distr,
                        std::function<double()> mass_distr) {
-    Particle p {
-        .x = pos_distr(),
-        .y = pos_distr(),
-        .z = pos_distr(),
-        .mass = mass_distr(),
-    };
+    Particle p { pos_distr(), pos_distr(), pos_distr(), mass_distr()};
     return p;
 }
 
@@ -54,10 +47,11 @@ void move_particle(Particle& p, double dx, double dy, double dz) {
 }
 
 double dist(const Particle& p1, const Particle& p2) {
+    using std::sqrt;
     return sqrt(sqr(p1.x - p2.x) + sqr(p1.y - p2.y) + sqr(p1.z - p2.z));
 }
 
-ostream& operator<<(ostream& out, const Particle& p) {
+std::ostream& operator<<(std::ostream& out, const Particle& p) {
     return out << "(" << p.x << ", " << p.y << ", " << p.z << ")"
                << ", mass = " << p.mass;
 }
