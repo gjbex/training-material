90520ae4db6322b9daac0fa017543d552ef1c598
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-05-27 14:11:16 +0200

    Add simple std::transform example
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- CPlusPlus/Algorithms/for_each.cpp
+++ CPlusPlus/Algorithms/for_each.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 int main() {
-    std::vector<double> v {{0.5, 0.75, 1.0, 1.5, 3.5}};
+    std::vector<double> v {0.5, 0.75, 1.0, 1.5, 3.5};
     std::for_each(v.begin(), v.end(), [] (double& x) { x *= x; });
     for (auto x: v)
         std::cout << x << " ";
