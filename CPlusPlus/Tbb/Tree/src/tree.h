7a4cfafa22b60105d95e0141b40584bf412652db
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-05-28 13:39:16 +0200

    Add logic to keep track of parent node
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- CPlusPlus/Tbb/Tree/src/tree.h
+++ CPlusPlus/Tbb/Tree/src/tree.h
@@ -10,16 +10,21 @@ class Node {
         T data_;
         std::unique_ptr<Node<T>> left_child_;
         std::unique_ptr<Node<T>> right_child_;
+        Node<T>* parent_;
+        void set_parent(Node<T>* parent) { parent_ = parent; }
     public:
-        Node(T data) : data_ {data}, left_child_ {nullptr}, right_child_ {nullptr} {};
+        Node(T data) : data_ {data}, left_child_ {nullptr}, right_child_ {nullptr},
+           parent_ {nullptr} {}
         T& operator()() { return data_; }
         Node<T>* left() { return left_child_.get(); }
         Node<T>* right() { return right_child_.get(); }
         void set_left(std::unique_ptr<Node<T>> child) {
             left_child_ = std::move(child);
+            left_child_->set_parent(this);
         }
         void set_right(std::unique_ptr<Node<T>> child) {
             right_child_ = std::move(child);
+            right_child_->set_parent(this);
         }
         friend std::ostream& operator<<(std::ostream& out, const Node<T>& node) {
             out << node.data_;
