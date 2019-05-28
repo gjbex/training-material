f10a308710f3c5bd91ce503291990e33c2fd3e48
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-05-28 13:40:57 +0200

    Add tbb::task based summation of node values
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- CPlusPlus/Tbb/Tree/src/main_tree_test.cpp
+++ CPlusPlus/Tbb/Tree/src/main_tree_test.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <memory>
 #include <random>
+#include <tbb/tbb.h>
 #include "tree.h"
+#include "sum_task.h"
 #include "tree_init.h"
 
 int main(int argc, char* argv[]) {
@@ -14,5 +16,10 @@ int main(int argc, char* argv[]) {
         seed = std::stoul(argv[2]);
     std::unique_ptr<Node_t> tree = gen_random_tree(child_prob, max_depth, seed);
     print_tree<double>(std::cout, tree.get());
+    std::cout << std::endl;
+    double sum {0.0};
+    auto task = new(tbb::task::allocate_root()) SumTask<double>(tree.get(), &sum);
+    tbb::task::spawn_root_and_wait(*task);
+    std::cout << "sum = " << sum << std::endl;
     return 0;
 }
