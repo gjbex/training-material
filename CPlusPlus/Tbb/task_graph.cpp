695755f5eae549bb34c0c2e03285aad4dd466ed7
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-06-05 07:52:11 +0200

    Simplify code by using lambdas
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- CPlusPlus/Tbb/task_graph.cpp
+++ CPlusPlus/Tbb/task_graph.cpp
@@ -3,38 +3,25 @@
 
 using namespace tbb::flow;
 
-struct Square {
-    int operator()(int v) { return v*v; }
-};
-
-struct Cube {
-    int operator()(int v) { return v*v*v; }
-};
-
-class Sum {
-    int &sum_;
-    public:
-    Sum( int& sum) : sum_(sum) {}
-    int operator()( tuple< int, int > v ) {
-        sum_ += get<0>(v) + get<1>(v);
-        return sum_;
-    }
-};
-
 int main() {
     int result = 0;
-
     graph g;
     broadcast_node<int> input(g);
-    function_node<int,int> Squarer(g, unlimited, Square());
-    function_node<int,int> Cuber(g, unlimited, Cube());
+    function_node<int,int> squarer(g, unlimited,
+            [] (int v) -> int { return v*v; });
+    function_node<int,int> cuber(g, unlimited,
+            [] (int v) -> int { return v*v*v; });
     join_node< tuple<int,int>, queueing> join( g );
-    function_node<tuple<int,int>,int> sum(g, serial, Sum(result));
-
-    make_edge(input, Squarer);
-    make_edge(input, Cuber);
-    make_edge(Squarer, get<0>(join.input_ports()));
-    make_edge(Cuber, get<1>( join.input_ports()));
+    function_node<tuple<int,int>,int> sum(g, serial,
+            [&result] (tuple<int,int> v) -> int {
+                result += get<0>(v) + get<1>(v);
+                return result;
+            }
+    );
+    make_edge(input, squarer);
+    make_edge(input, cuber);
+    make_edge(squarer, get<0>(join.input_ports()));
+    make_edge(cuber, get<1>( join.input_ports()));
     make_edge(join, sum);
 
     for (int i = 1; i <= 10; ++i)
