3d581627d58f42b43b4014a52b1c533ba2a5ebfc
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-06-21 15:12:41 +0200

    Add example of passing a numpy array to a C++ function
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- Python/Interfacing_C_C++_Fortran/Pybind11/Stats/test.py
+++ Python/Interfacing_C_C++_Fortran/Pybind11/Stats/test.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
-from stats import Statistics
+import numpy as np
+from stats import Statistics, compute_stats
 
+n = 10
+data = np.empty(n)
 my_stats = Statistics()
-for i in range(10):
-    my_stats.add(0.3*i)
-print(my_stats.n())
+for i in range(n):
+    value = 0.3*i
+    my_stats.add(value)
+    data[i] = value
+print(my_stats.mean())
+my_stats = compute_stats(data)
+print(my_stats.mean())
