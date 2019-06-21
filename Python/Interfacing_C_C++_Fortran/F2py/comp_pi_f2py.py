df215e1e574b6eeab23643b0f9077cc07e04a98c
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-06-21 10:36:20 +0200

    improve Python style
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- Python/Interfacing_C_C++_Fortran/F2py/comp_pi_f2py.py
+++ Python/Interfacing_C_C++_Fortran/F2py/comp_pi_f2py.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 
+from argparse import ArgumentParser
 import sys
 from comp_pi import compute_pi
 
 
 def main():
-    n = int(sys.argv[1]) if len(sys.argv) > 1 else 1000
-    print(compute_pi(n))
+    arg_parser = ArgumentParser(description='compute pi using Fortran '
+                                'function')
+    arg_parser.add_argument('n', default=1000, nargs='?',
+                            help='number of random points')
+    options = arg_parser.parse_args()
+    print(compute_pi(options.n))
     return 0
 
 if __name__ == '__main__':
