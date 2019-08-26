263a13e68dce3725eae55371f12f8b7f8423ddb3
Geert Jan Bex <geertjan.bex@uhasselt.be>
2019-08-26 13:00:54 +0200

    Complete terrain application
    Why:
    
    *
    
    This change addresses the need by:
    
    *

--- CPlusPlus/Tbb/LineOfSight/src/terrain_main.cpp
+++ CPlusPlus/Tbb/LineOfSight/src/terrain_main.cpp
@@ -9,6 +9,20 @@ int main(int argc, char* argv[]) {
     double distance {30.0};
     if (argc > 2)
         distance = std::stof(argv[2]);
-
+    double delta_mean {0.02};
+    if (argc > 3)
+        delta_mean = std::stof(argv[3]);
+    double delta_stddev {0.05};
+    if (argc > 4)
+        delta_stddev = std::stof(argv[4]);
+    double flat_fraction {0.2};
+    if (argc > 5)
+        flat_fraction = std::stof(argv[5]);
+    Seed_t seed {1234};
+    if (argc > 6)
+        seed = std::stoul(argv[6]);
+    Terrain terrain(n, distance, delta_mean, delta_stddev, flat_fraction, seed);
+    for (size_t i = 0; i < terrain.n(); ++i)
+        std::cout << terrain.distance(i) << " " << terrain.altitude(i) << std::endl;
     return 0;
 }
